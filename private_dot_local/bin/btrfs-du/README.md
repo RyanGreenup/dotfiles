17a4e6dfde7288ec21cbea82b89a19a3e28d5254
Ryan G <exogenesis@protonmail.com>
2021-08-13 11:39:46 +1000

    [btrfs] add script to list subvol sizes

---
+++ private_dot_local/bin/btrfs-du/README.md
@@ -0,0 +1,57 @@
+# BTRFS subvolume/snapshot size with paths
+> This was forked from (btrfs-df)[https://github.com/stecman/btrfs-df] and modified to have help printout so i can remember how to use it.
+
+List BTRFS subvolume space use information similar to `df -h` with paths displayed.
+
+Normally, `btrfs qgroup show` lists subvolumes, but only by id:
+
+```
+$ btrfs qgroup show /mnt/somebtrfsvol --human-readable --sort=-excl
+
+qgroupid         rfer         excl
+--------         ----         ----
+0/2317      848.55GiB     24.35GiB
+0/3217      883.62GiB     16.04GiB
+0/1998      840.20GiB     16.01GiB
+0/489       160.81GiB      8.01GiB
+0/7125      227.62GiB      2.21GiB
+0/7202      230.20GiB    982.49MiB
+```
+
+This script saves looking up path information with `btrfs subvolume list` as paths
+are included added the output:
+
+```
+$ ./btrfs-subvolumes.py /mnt/somebtrfsvol --human-readable --sort=-excl
+
+path                                        qgroupid         rfer         excl
+----                                        --------         ----         ----
+Photos/Photos.20160703                      0/2317      848.55GiB     24.35GiB
+Photos/Photos.20161106                      0/3217      883.62GiB     16.04GiB
+Photos/Photos.20160605                      0/1998      840.20GiB     16.01GiB
+Archive/Archive.20160504                    0/489       160.81GiB      8.01GiB
+timemachine/timemachine.20170716            0/7125      227.62GiB      2.21GiB
+timemachine/timemachine.20170723            0/7202      230.20GiB    982.49MiB
+```
+
+## Usage
+
+For this to work on a BTRFS volume, you first need to enable quotas on the volume:
+
+```bash
+btrfs quota enable /mnt/some-volume
+```
+
+All arguments are passed through to `btrfs qgroup show`, so you can get started with:
+
+```bash
+./btrfs-subvolumes.py /mnt/some-volume -h
+```
+
+## Notes
+
+The current version of this script does not allow sorting by path as it passes
+all arugments through to btrfsprogs. If you need sorting by path, either submit a PR
+or use the original version of the script [here](https://github.com/stecman/btrfs-df/commit/096f480cad6ba5c0573d9523093195a1e33f5808).
+
+This was originally based on [a shell script that was too slow](https://github.com/agronick/btrfs-size).
---
+++ private_dot_local/bin/btrfs-du/executable_btrfs-subvolumes.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+
+# List BTRFS subvolume space use information similar to df -h (with snapshot paths)
+#
+# Btrfsprogs is able to list and sort snapshots on a volume, but it only prints their
+# id, not their path. This script wraps `btrfs qgroup show` to add filesystem paths
+# to the generated table.
+#
+# For this to work on a BTRFS volume, you first need to enable quotas on the volume:
+#
+#    btrfs quota enable /mnt/some-volume
+#
+# Note that the current version of this script does not allow sorting by path, as it
+# passes all arugments through to btrfsprogs. If you need that and don't mind being
+# limited to only sorting by path, see this previous version:
+#
+#    https://gist.github.com/stecman/3fd04a36111874f67c484c74e15ef311/6690edbd6a88380a1712024bb4115969b2545509
+#
+# This is based on a shell script that was too slow:
+# https://github.com/agronick/btrfs-size
+
+from __future__ import print_function
+
+import subprocess
+import sys
+import os
+import re
+
+# MAIN .....................................................
+
+def main():
+    get_args()
+    # Re-run the script as root if started with a non-priveleged account
+    if os.getuid() != 0:
+        cmd = 'sudo "' + '" "'.join(sys.argv) + '"'
+        sys.exit(subprocess.call(cmd, shell=True))
+
+
+    # Fetch command output to work with
+    output = get_data_raw(sys.argv[1:])
+    subvols = get_btrfs_subvols(guess_path_argument(sys.argv))
+
+    # Data for the new column
+    path_column = [
+        "path",
+        "----"
+    ]
+
+    # Iterate through all lines except for the table header
+    for index,line in enumerate(output):
+        # Ignore header rows
+        if index <= 1:
+            continue
+
+        groupid = get_qgroup_id(line)
+
+        if groupid in subvols:
+            path_column.append(subvols[groupid])
+        else:
+            path_column.append("")
+
+    # Find the required width for the new column
+    column_width = len(max(path_column, key=len)) + 2
+
+    # Output data with extra column for path
+    for index,line in enumerate(output):
+        if path_column[index]=="":
+            # We can't print anything useful for qgroups that aren't associated with a path
+            continue
+
+        print(path_column[index].ljust(column_width) + output[index])
+
+# Helper Functions..........................................
+
+def get_args():
+    if len(sys.argv) < 2: 
+        print("Incorrect number of arguments")
+        print_help()
+        sys.exit(1)
+    if sys.argv[1] in ["-h", "--help"]:
+        print_help()
+        sys.exit(0)
+    
+def print_help():
+    print(sys.argv[0]+"\t [OPTIONS] [path to btrfs subvol]\n")
+    print("Example\n")
+    print(sys.argv[0]+"\t --sort excl / \n")
+    print("qgroups will need to be enabled, these can be\n"+
+        "enabled with something like:\n\t sudo btrfs quota enable /mnt/@")
+
+def get_btrfs_subvols(path):
+    """Return a dictionary of subvolume names indexed by their subvolume ID"""
+    try:
+        raw = subprocess.check_output(["btrfs", "subvolume", "list", path])
+        volumes = re.findall(r'^ID (\d+) .* path (.*)$', raw.decode("utf8"), re.MULTILINE)
+
+        return dict(volumes)
+
+    except subprocess.CalledProcessError as e:
+        if e.returncode != 0:
+            print("\nFailed to list subvolumes")
+            print("Is '%s' really a BTRFS volume?" % path)
+            sys.exit(1)
+
+def get_data_raw(args):
+    """Return lines of output from a call to 'btrfs qgroup show' with args appended"""
+    try:
+        # Get the lines of output, ignoring the two header lines
+        raw = subprocess.check_output(["btrfs", "qgroup", "show"] + args)
+        return raw.decode("utf8").split("\n")
+
+    except subprocess.CalledProcessError as e:
+        if e.returncode != 0:
+            print("\nFailed to get subvolume quotas. Have you enabled quotas on this volume?")
+            print("(You can do so with: sudo btrfs quota enable <path-to-volume>)")
+            sys.exit(1)
+
+def get_qgroup_id(line):
+    """Extract qgroup id from a line of btrfs qgroup show output
+    Returns None if the line wasn't valid
+    """
+    id_match = re.match(r"\d+/(\d+)", line)
+
+    if not id_match:
+        return None
+
+    return id_match.group(1)
+
+def guess_path_argument(argv):
+    """Return an argument most likely to be the <path> arg for 'btrfs qgroup show'
+    This is a cheap way to pass through to btrfsprogs without duplicating the options here.
+    Currently only easier than duplication because the option/argument list is simple.
+    """
+    # Path can't be the first argument (program)
+    args = argv[1:]
+
+    # Filter out arguments to options
+    # Only the sort option currently takes an argument
+    option_follows = [
+        "--sort"
+    ]
+
+    for text in option_follows:
+        try:
+            position = args.index(text)
+            del args[position + 1]
+        except:
+            pass
+
+    # Ignore options
+    args = [arg for arg in args if re.match(r"^-", arg) is None]
+
+    # Prefer the item at the end of the list as this is the suggested argument order
+    return args[-1]
+
+
+if __name__ == "__main__":
+    main()
