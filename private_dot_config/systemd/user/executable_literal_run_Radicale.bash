865be76c582e218cfafc6917733ea47bf542611e
Ryan G <exogenesis@protonmail.com>
2021-08-18 16:56:25 +1000

    Test for password file existing

--- private_dot_config/systemd/user/executable_literal_run_Radicale.bash
+++ private_dot_config/systemd/user/executable_literal_run_Radicale.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Change Directory
-dir="$HOME/.local/radicale"
+dir="$HOME/.local/radicale/"
+pass_file="radicale_password.passwd"
 
 if [[ -d "${dir}" ]]
 then
@@ -10,6 +11,16 @@ else
 	mkdir "${dir}"
 	cd "${dir}"
 fi
+
+if [[ ! -f "${pass_file}" ]]
+then
+	echo "No Password File Found, looking for ${dir}${pass_file}"
+	echo "Fix this by running:"
+	echo "htpasswd -c ${dir}${pass_file} [username]"
+	exit 1
+fi
+
+
 
 
 radicale -H 0.0.0.0:5232                                      \
