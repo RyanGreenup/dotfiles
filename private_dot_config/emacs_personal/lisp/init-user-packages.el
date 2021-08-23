063357c99f7f2c88d8b689008e34f522ca664093
Ryan G <exogenesis@protonmail.com>
2021-08-23 16:01:44 +1000

    [emacs] Add Org Roam UI

--- private_dot_config/emacs_personal/lisp/init-user-packages.el
+++ private_dot_config/emacs_personal/lisp/init-user-packages.el
@@ -58,6 +58,21 @@
 ;;;;; Org Roam Server
 (straight-use-package 'org-roam-server)
 
+;;;;; Org Roam UI (Better org roam server)
+;; https://github.com/org-roam/org-roam-ui#straightuse-package
+(use-package org-roam-ui
+  :straight
+    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
+    :after org-roam
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
 ;;;; Emacs Application Framework
 ;;;; Interface
 ;;;; Macros
