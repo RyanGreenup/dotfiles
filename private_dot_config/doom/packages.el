e52bc4d8edcfda5c21990e4b938c2a140bc7478a
Ryan G <exogenesis@protonmail.com>
2022-01-15 16:03:40 +1100

    [Doom Emacs] Update doom init and doom packages

--- private_dot_config/doom/packages.el
+++ private_dot_config/doom/packages.el
@@ -1,66 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here, run 'doom sync' on
-;; the command line, then restart Emacs for the changes to take effect.
-;; Alternatively, use M-x doom/reload.
-
-(package! dash)
-(package! hyperbole)
-(package! simple-httpd)
-(package! outshine)
-(package! texfrag)
-(package! org-pdfview)
-(package! helm-org-rifle)
-(package! org-ql)
-(package! helm-org)
-(package! texfrag)
-(package! ob-mongo)
-(package! leuven-theme)
-(package! magic-latex-buffer)
-(package! mediawiki)
-(package! emoji-fontset)
-(package! emoji-display)
-;; (package! org-superstar)
-
-; These make dokuwiki easier to work with
-
-;(package! math-preview)  ; this isn't working for some reason
-(package! dokuwiki-mode)
-(package! ox-wk)
-;; I think Doom comes with counsel recoll
-;; (package! helm-recoll)
-
-;; Both of these must be here otherwise poly mode crashes all the time
-(package! polymode)
-(package! org-roam-server)
-(package! poly-R)
-(package! org-ref)
-(package! citeproc) ; I don't think I need this anymore, just export -> latex -> HTML (pandoc) to keep refs
-(package! elmacro)
-(package! figlet) ; Makes nice headings
-
-;; Slides
-;(package! ox-reveal)
-
-;; Temporary
-(package! lsp-julia)  ;; This will eventually be in doom
-(package! eglot-jl)  ;; This will eventually be in doom
-
-;; Doom's packages are pinned to a specific commit and updated from release to
-;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
-;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
-;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
-;; To install a package directly from a particular repo, you'll need to specify
-;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
@@ -72,8 +22,8 @@
 ;  :recipe (:host github :repo "username/repo"
 ;           :files ("some-file.el" "src/lisp/*.el")))
 
-;; If you'd like to disable a package included with Doom, for whatever reason,
-;; you can do so here with the `:disable' property:
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
 ;(package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
@@ -86,3 +36,27 @@
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
+
+;; Use `:pin' to specify a particular commit to install.
+;(package! builtin-package :pin "1a2b3c4d5e")
+
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;(unpin! pinned-package)
+;; ...or multiple packages
+;(unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;(unpin! t)
+
+;; (use-package org-logseq
+;;     :straight (org-logseq :fetcher github :repo "llcc/org-logseq" :files ("*"))
+;;     :custom (org-logseq-dir "~/logseq"))
+
+
+(package! org-logseq
+ :recipe (:host github :repo "llcc/org-logseq"
+          :files ("*")))
+
+(package! outshine)
+(package! texfrag)
