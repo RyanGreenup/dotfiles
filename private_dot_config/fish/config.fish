5fe3fe4a97eb2f4bfff836908f7a81eed0cd4e3e
Ryan G <exogenesis@protonmail.com>
2022-01-17 15:14:44 +1100

    Clean up Fish Script

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -6,74 +6,112 @@ set PATH $HOME/go/bin $PATH
 
 # Set Default Editor to Emacs
 # set VISUAL 'emacs -nw --eval "(add-hook \'emacs-startup-hook #\'sh-mode)"'
-# set VISUAL 'code'
-set VISUAL 'nvim'
+set VISUAL nvim
 
+# ..............................................................................
+# * Better Coreutils / Built-ins................................................
+# ..............................................................................
 # Defined in - @ line 1
 function f --wraps='cd ; exa -RGL 3' --description 'alias f=cd; exa -RGL 3'
-  cd $argv ; exa -TL 2 ; exa ;
+    cd $argv
+    exa -TL 2
+    exa
 end
 
-
-## Search for installable Packages
-function f --wraps='cd ; exa -RGL 3' --description 'alias f=cd; exa -RGL 3'
-  cd $argv ; exa -TL 2 ; exa ;
+function l --wraps='cd ; exa -RGL 3' --description 'alias f=cd; exa -RGL 3'
+    cd $argv
+    exa -TL 2
+    exa
 end
 
 
 ## Better LS
 function ls! --wraps='ls -ultrah' --description 'alias ls!=ls -ultrah'
-  ls -ultrah $argv;
+    ls -ultrah $argv
 end
 
 
 ## Easier Xclip
-function x --wraps='xclip -selection clipboard' --description 'alias x=xclip -selection clipboard'
-  xclip -selection clipboard $argv;
+function x --wraps='xclip -selection clipboard' --description 'Alias for xclip'
+    xclip -selection clipboard $argv
+end
+
+# ..............................................................................
+# * Notetaking Stuff ...........................................................
+# ..............................................................................
+
+
+# ** Searching .................................................................
+function _private_search
+    set notes_dir $argv
+
+    nvim (
+        sk -m -i -c "note_taking search -d "$notes_dir"  {}"        \
+            --bind pgup:preview-page-up,pgdn:preview-page-down      \
+            --preview "bat --style grid --color=always              \
+                            --terminal-width 80 $notes_dir/{+}      \
+                            --italic-text=always                    \
+                            --decorations=always"                |  \
+        sed "s#^#$notes_dir/#"
+    )
+end
+
+# *** Search New notes
+function ns
+    _private_search ~/Notes
+end
+
+# *** Search ALL notes
+# I symlinked ~/Notes under ~/Sync/Notes to catch it in this (excludes dokuwiki though)
+function nS
+    _private_search ~/Sync/Notes
+end
+
+# ** Finding ......................................................................
+## I could have used `note_taking fzf` but skim and bat is prettier
+function _private_finding
+    fd '\.org$|\.md$' $argv |
+        sk --ansi -m -c 'rg -l -t markdown -t org -t txt --ignore-case "{}"' \
+            --preview "bat --style snip {} 2> /dev/null --color=always" \
+            --bind 'ctrl-f:interactive,pgup:preview-page-up,pgdn:preview-page-down'
+end
+
+# *** Find main notes
+function nf --description 'Find Notes'
+    set _notes (_private_finding ~/Notes) \
+        && nvim $_notes
+end
+
+# *** Find ALL notes
+function nF
+    # Find the notes and open if not cancelled
+    set _notes (_private_finding ~/Notes ~/Sync/Notes /srv/http/dokuwiki/) \
+        && nvim $_notes
 end
 
 
-## Cadmus Bindings
-function ns --wraps='cadmus note search' --description 'alias ns=cadmus note search'
-  cadmus note search $argv;
-end
+# ..............................................................................
+# * Package Management Stuff....................................................
+# ..............................................................................
 
-
-function nf --wraps='cadmus note find' --description 'alias ns=cadmus note find'
-  cadmus note find $argv;
-end
-
-
-function wa --wraps='cadmus misc w' --wraps='cadmus misc wa' --description 'alias wa=cadmus misc wa'
-  cadmus misc wa $argv;
-end
-
-
-function wr --wraps='cadmus misc w' --wraps='cadmus misc waR' --description 'alias wr=cadmus misc wR'
-  cadmus misc wR $argv;
-end
-
-function no --wraps='cadmus misc code' --description 'alias no=cadmus misc code'
-  cadmus misc code $argv;
-end
-
-function ng --wraps='cadmus misc atom' --description 'alias ng=cadmus misc atom'
-  cadmus misc atom $argv;
-end
-
-## Use fzf to install packages
-
+# Packages in Repository
 function pz --description 'Fuzzy Find to preview and install with pacman'
-  pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S $argv;
+    pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S $argv
 end
 
-function wk --description 'Alias for work script' --wraps='workdispatch'
-  workdispatch
+# All Available Packages
+function pZ --description 'Fuzzy Find to preview and install with pacman'
+    paru -Slq | fzf --multi --preview 'paru -Si {1}' | xargs -ro sudo paru -S $argv
 end
+
+# Open work Dispatcher
+function wk --description 'Alias for work script' --wraps='workdispatch'
+    emacsclient --create-frame ~/Agenda/todo.org ~/Agenda/projects.org & disown
+end
+
 
 
 # ## Make sure to reflink btrfs
-# 
 # function cp --description 'use reflink auto for deduped in btrfs' --wraps='cp'
 #   cp --reflink=auto $argv;
 # end
