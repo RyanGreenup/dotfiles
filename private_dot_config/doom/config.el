a1a6c12f442ea15cb39803eb9b03d678a5804f0e
ryangreenup <exogenesis@protonmail.com>
2022-02-03 17:16:54 +1100

    Add org-ref to doom emacs

--- private_dot_config/doom/config.el
+++ private_dot_config/doom/config.el
@@ -139,3 +139,36 @@
     (texfrag-mode)
   )
 )
+;;; Org Ref Stuff
+
+
+(setq my/bib_locations '("~/Notes/ref.bib"))
+(setq my/bib_notes_location "~/Notes/refs/")
+(after! org
+  (require 'org-ref-ivy)
+
+  (setq org-ref-insert-link-function 'org-ref-insert-link-hydra/body
+        org-ref-insert-cite-function 'org-ref-cite-insert-ivy
+        org-ref-insert-label-function 'org-ref-insert-label-link
+        org-ref-insert-ref-function 'org-ref-insert-ref-link
+        org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body)))
+
+  (setq bibtex-completion-bibliography 'my/bib_locations
+        ;; bibtex-completion-library-path '("~/Zotero/storage/")
+        bibtex-completion-notes-path 'my/bib_notes_location
+        bibtex-completion-additional-search-fields '(keywords)
+        bibtex-completion-pdf-open-function
+          (lambda (fpath)
+                  (call-process "zathura" nil 0 nil fpath)))
+
+
+  (define-key org-mode-map (kbd "C-c ]") 'org-ref-insert-link))
+
+;;; Set the org-mode latex backend
+(after! org
+  ; LaTeXMk is more efficient
+  ;; Execute either depending on which one you want, and or swap order
+  (setq org-latex-listings 'minted
+    org-latex-packages-alist '(("" "minted"))
+    org-latex-pdf-process
+    '("latexmk -8bit -f -shell-escape -xelatex -interaction=nonstopmode %F")))
