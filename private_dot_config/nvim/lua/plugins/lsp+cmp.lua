1f7188444b309616e4352e2116eaa55bff32b9ba
Ryan G <exogenesis@protonmail.com>
2022-03-28 00:11:02 +1100

    add F8 for jumping

--- private_dot_config/nvim/lua/plugins/lsp+cmp.lua
+++ private_dot_config/nvim/lua/plugins/lsp+cmp.lua
@@ -69,6 +69,7 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<F8>', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
