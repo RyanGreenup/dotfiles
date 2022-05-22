29cc7f206c79020b21819454573f7a8455beba7c
Ryan G <exogenesis@protonmail.com>
2022-05-22 17:44:09 +1000

    [qutebrowser] add darkmode

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -16,3 +16,25 @@ c.tabs.position = 'left'
 ##   - multiple: Hide the tab bar if only one tab is open.
 ##   - switching: Show the tab bar when switching tabs.
 c.tabs.show = 'multiple'
+# colors.webpage.prefers_color_scheme_dark
+
+
+
+## Styling
+import styles.dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+styles.dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
+
+
+
+## Optional Dark mode
+config.bind('td', 'config-cycle colors.webpage.darkmode.enabled ;; restart')
+config.bind('tj', 'config-cycle content.javascript.enabled')
