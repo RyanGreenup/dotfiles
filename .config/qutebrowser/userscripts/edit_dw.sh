d935d47ab7f2f5e259802517d906938f55720aa2
Ryan G <exogenesis@protonmail.com>
2022-06-23 13:36:13 +1000

    [qutebrowser] ed now works on anchor-tags

--- .config/qutebrowser/userscripts/edit_dw.sh
+++ .config/qutebrowser/userscripts/edit_dw.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/sh
 
-editor='emacsclient --create-frame'
+# editor='emacsclient --create-frame'
+editor='gnvim'
 
+# sd calls:
+#  1. remove #anchor-tags
+#  2. extract id
+#  3. replace ':' with '/'
+#  4. prend the file path
 file="$(echo "${QUTE_URL}" |\
+     sd '.*id=(.*)#.*$' 'id=$1' |\
      sd '.*id=(.*)' '$1.txt' |\
      sd ':' '/' | tr -d '\n' |\
      sd '^' "$HOME/Notes/dokuwiki/data/pages/")"
