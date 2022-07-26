496cb14b395f138f6a78e77cbcb7b8f0d7fead00
Ryan G <exogenesis@protonmail.com>
2022-07-26 18:49:08 +1000

    Add script for counting files

--- .local/bin/count_files.sh
+++ .local/bin/count_files.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-for dir in */; do                       # use globbing to list dirs
-    cd "${dir}" 2>/dev/null || continue # if unable to cd, move on
-    dir=${dir%*/}                       # Remove the trailing /
-    count=$(find . -type f | wc -l)
-    printf '%10d : %s\n' "${count}" "${dir}"
-    cd ..
-done | sort -nr
+count_files() {
+    for dir in */; do                       # use globbing to list dirs
+        cd "${dir}" 2>/dev/null || continue # if unable to cd, move on
+        dir=${dir%*/}                       # Remove the trailing /
+        count=$(fd -u -t f| wc -l)
+        # count=$(find . -type f | wc -l)
+        printf '%10d : %s\n' "${count}" "${dir}"
+        cd ..
+    done | sort -nr
+}
 
+count_files_rec() {
+    for dir in */; do                       # use globbing to list dirs
+        d="${1}"
+        cd "${dir}" 2>/dev/null || continue # if unable to cd, move on
+        dir=${dir%*/}                       # Remove the trailing /
+        count=$(find . -type f | wc -l)
+        printf "%${1}d : %s\n" "${count}" "${dir}"
+        count_files_rec $((d+4))
+        cd ..
+    done # | sort -nr
+}
+
+# count_files_rec 4
+count_files
