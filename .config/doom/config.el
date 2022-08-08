e6781d1f90aad7085a2a05d1d72687d59b3615d4
Ryan G <exogenesis@protonmail.com>
2022-08-08 16:18:14 +1000

    [emacs] adjust agenda files

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -30,7 +30,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 ;; (setq org-directory "~/Agenda/")
-(setq org-agenda-files (list "~/Agenda" "~/Notes/slipbox/journals"))
+(setq org-agenda-files (list "~/Agenda"))
 
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
