1687b71a92aae5544ce87ef5efdcb6076726389d
Ryan G <exogenesis@protonmail.com>
2022-08-09 20:18:01 +1000

    [nvim] began cleaning up keybindings

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,34 +1,112 @@
 local wk = require('which-key')
 
+vim.cmd [[ set timeoutlen=10 ]]
+
 wk.register({
   o = {
     name = "Org", -- optional group name
-    a = {"Agenda"},
-    A = {"Archive"},
-    c = {"Capture"},
-    t = {"Set Tag"},
-    r = {"Refile"},
-    o = {"Open at Point"},
-    k = {"Cancel Edit Source"},
-    w = {"Save Edit Source"},
-    K = {"Move Subtree Up"},
-    J = {"Move Subtree Down"},
-    e = {"Export"},
+    a = { "Agenda" },
+    A = { "Archive" },
+    c = { "Capture" },
+    t = { "Set Tag" },
+    r = { "Refile" },
+    o = { "Open at Point" },
+    k = { "Cancel Edit Source" },
+    w = { "Save Edit Source" },
+    K = { "Move Subtree Up" },
+    J = { "Move Subtree Down" },
+    e = { "Export" },
     i = {
       name = "Insert",
-      d = {"Deadline"},
-      h = {"Heading"},
-      T = {"Mark TODO"},
-      t = {"Toggle Below"},
-      s = {"Schedule"}
+      d = { "Deadline" },
+      h = { "Heading" },
+      T = { "Mark TODO" },
+      t = { "Toggle Below" },
+      s = { "Schedule" }
     },
     x = {
       name = "Clock",
-      o = {"Clock Out"},
-      i = {"Clock In"},
-      q = {"Cancel Clock"},
-      j = {"Jump to Clock"},
-      e = {"Set Effort"},
+      o = { "Clock Out" },
+      i = { "Clock In" },
+      q = { "Cancel Clock" },
+      j = { "Jump to Clock" },
+      e = { "Set Effort" },
     }
   },
 }, { prefix = "<leader>" })
+
+
+
+wk.register({
+  ["<leader>"] = {
+    f = {
+      name = "+file",
+      f = { "<cmd>Telescope find_files<cr>", "Find File" },
+      r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+      n = { "<cmd>enew<cr>", "New File" },
+    },
+    h = {
+      name = "help",
+      r = { "<cmd>source $MYVIMRC<cr>", "source $MYVIMRC" },
+      u = { "<cmd>:PackerSync<CR>", "Packer Sync" },
+    },
+    o = {
+      name = "Org", -- optional group name
+      a = { "Agenda" },
+      A = { "Archive" },
+      c = { "Capture" },
+      t = { "Set Tag" },
+      r = { "Refile" },
+      o = { "Open at Point" },
+      k = { "Cancel Edit Source" },
+      w = { "Save Edit Source" },
+      K = { "Move Subtree Up" },
+      J = { "Move Subtree Down" },
+      e = { "Export" },
+      i = {
+        name = "Insert",
+        d = { "Deadline" },
+        h = { "Heading" },
+        T = { "Mark TODO" },
+        t = { "Toggle Below" },
+        s = { "Schedule" }
+      },
+      x = {
+        name = "Clock",
+        o = { "Clock Out" },
+        i = { "Clock In" },
+        q = { "Cancel Clock" },
+        j = { "Jump to Clock" },
+        e = { "Set Effort" },
+      }
+    },
+    t = {
+      name = "Toggle",
+      a = { "<cmd>ASToggle<CR>:lua require('notify')('Toggled Autosave')<CR>", "Autosave" },
+      n = { "<cmd>lua require('notify').dismiss()<CR>", "Dismiss notifications" },
+      g = { "<cmd>GoldenRatioToggle<CR>", "Golden Ratio"}
+    },
+    i = {
+      name = "insert",
+      u = { "<cmd>Telescope symbols<CR>", "Symbols" },
+    },
+    w = {
+      name = "Window",
+      -- Splits
+      v = { "<cmd>:vsplit<CR>",  "vsplit"      },
+      s = { "<cmd>:split<CR>",   "hsplit"      },
+      h = { "<C-w>h",            "Move Left"   },
+      j = { "<C-w>j",            "Move Down"   },
+      k = { "<C-w>k",            "Move Up"     },
+      l = { "<C-w>l",            "Move Right"  },
+      d = { "<cmd>q<CR>",        "quit"        },
+      q = { "<cmd>:q<CR>",       "quit"        },
+      Q = { "<cmd>qall!<CR>",    "Quit all!"   },
+  ["="] = { "<C-w>=",    "Quit all!"   },
+    },
+  ["<Left>"] =  { "<cmd>vertical resize +15<CR>", "Resize" },
+  ["<Right>"] = { "<cmd>vertical resize -15<CR>", "Resize" },
+  ["<Up>"] =    { "<cmd>         resize +15<CR>", "Resize" },
+  ["<Down>"] =  { "<cmd>         resize -15<CR>", "Resize" },
+  },
+})
