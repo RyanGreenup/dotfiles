4c1a63cde662d5001ca502c4ba546a1a77fd0589
Ryan G <exogenesis@protonmail.com>
2023-01-23 11:34:31 +1100

    [nvim] add neovide config

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -203,18 +203,18 @@ if exists('g:neovide')
   let g:neovide_floating_blur_amount_y = 200.0
 
 
-  let g:neovide_scroll_animation_length = 0.3
-  let g:neovide_hide_mouse_when_typing = v:true
+  "" let g:neovide_scroll_animation_length = 0.3
+  "" let g:neovide_hide_mouse_when_typing = v:true
 
-  let g:neovide_cursor_animation_length=0.05
-  let g:neovide_cursor_trail_size = 1.2
-  let g:neovide_cursor_antialiasing = v:true
+  "" let g:neovide_cursor_animation_length=0.05
+  "" let g:neovide_cursor_trail_size = 1.2
+  "" let g:neovide_cursor_antialiasing = v:true
 
   let g:neovide_cursor_vfx_mode = "railgun"
 
-  let g:neovide_cursor_vfx_particle_density = 15
-  let g:neovide_cursor_vfx_particle_lifetime = 1.2
-  let g:neovide_cursor_vfx_particle_speed = 10.0
+  "" let g:neovide_cursor_vfx_particle_density = 15
+  "" let g:neovide_cursor_vfx_particle_lifetime = 1.2
+  "" let g:neovide_cursor_vfx_particle_speed = 10.0
   "" let g:neovide_transparency = 0.8
 endif
 ]]
