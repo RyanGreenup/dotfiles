044fcf22e2931f4f27e2ce7a9b8c78748be238d1
Ryan G <exogenesis@protonmail.com>
2023-03-01 00:44:41 +1100

    [emacs] change agenda span and remove time grid

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -153,15 +153,19 @@
 (map! :leader
       :desc "Open Logseq Contents" "l c" #'org-logseq-toggle-contents-sidebar)
 (after! org
-  (setq org-agenda-span 7
-        org-agenda-start-on-weekday nil
-        org-agenda-start-day "-3d")
+  (setq
+        org-agenda-span 7
+        ;; org-agenda-start-day "-3d"
+        org-agenda-start-on-weekday 1 ; 0 is sunday, 1 is monday ...
+        calendar-week-start-day     1 ; What the first day of subsequent agenda views is
+        org-agenda-start-on-weekday nil)
   (setq org-tags-column 80))
 
- ;; (setq org-agenda-time-grid
- ;;         '((daily today weekly require-timed)
- ;;         (800 900 1000 1200 1300 1400  1500 1600 1700 1800 1900 2000)
- ;;         "......" "----------------"))
+
+;;  (setq org-agenda-time-grid
+;;          '((daily today weekly require-timed)
+;;          (800 900 1000 1200 1300 1400  1500 1600 1700 1800 1900 2000)
+;;          "......" "----------------"))
 ;; See also
 ;; (require 'calfw-org)
 
