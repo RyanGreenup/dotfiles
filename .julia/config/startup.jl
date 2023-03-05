71c0d6c2e9d913d3ee41c94833138ee363bcd7a0
Ryan G <exogenesis@protonmail.com>
2023-03-05 14:30:40 +1100

    [julia] startup sysimage

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,3 +1,31 @@
+# FezzikAutoGenStart
+# to remove remove entire block
+try
+    using Fezzik
+    try
+        Fezzik.trace()
+    catch err
+        @info "Something went wrong" err
+    end
+catch e
+    try
+        using Pkg
+        Pkg.add("Fezzik")
+        import Fezzik
+        try
+            Fezzik.trace()
+        catch err
+            @info "Something went wrong" err
+        end
+    catch err
+        @info "Something went wrong" err
+    end
+end
+
+# FezzikAutoGenEnd
+
+
+
 # print("Current Working directory is:", pwd())
 #=
 This conflicts with Iron
@@ -9,3 +37,6 @@ atreplinit() do repl
     end
 end
 =#
+
+
+
