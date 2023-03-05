18c20e734747e61d97e3d765a6b0da8246136748
Ryan G <exogenesis@protonmail.com>
2023-03-05 14:29:59 +1100

    [lf] basic settings

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -6,7 +6,7 @@ case "$1" in
     *.rar) unrar l "$1";;
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *.md) mdcat "$1" -;;
-    *.markdown) mdcat "$1" -;;
+    *.md) bat --color=always "$1" -;;
+    *.markdown) bat --color=always "$1" -;;
     *) highlight -O ansi "$1";;
 esac
