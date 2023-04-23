fe2bb20648d751197215706107edd55543ce4576
Ryan G <exogenesis@protonmail.com>
2023-04-23 14:43:06 +1000

    removed atuin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,15 +105,6 @@ if status is-interactive
     zoxide init fish | source
 end
 
-if status is-interactive
-    set -gx ATUIN_NOBIND "true"
-    atuin init fish | source
-
-    # bind to ctrl-r in normal and insert mode, add any other bindings you want here too
-    bind \cr _atuin_search
-    bind -M insert \cr _atuin_search
-end
-
 function lfcd
     set tmp (mktemp)
     lf -last-dir-path=$tmp $argv
