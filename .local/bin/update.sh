8d95f7e024592b9e50229fbf0bc0325d2e0ca78e
Ryan G <exogenesis@protonmail.com>
2023-09-13 16:44:05 +1000

    fix update script

--- .local/bin/update.sh
+++ .local/bin/update.sh
@@ -1,6 +1,9 @@
 # update nix
 nix-channel --update
 nix-env --upgrade
-nix-env -if default.nix
+nix-env -if /home/ryan/.config/nixpkgs/default.nix
+nix-store --gc
+nix-store --optimise
 # Update Emerge
+doas emaint --auto sync
 doas emerge --update --deep --newuse @world
