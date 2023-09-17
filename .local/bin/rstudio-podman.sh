44963491e4da44f5dfcf6769fd377baa91ccc401
ryan <exogenesis@protonmail.com>
2023-09-17 18:02:34 +1000

    podman script for rstudio

---
+++ .local/bin/rstudio-podman.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Check if an argument was provided
+if [ "$#" -eq 1 ]
+then
+  # If an argument was provided, we want interactive
+  opt="-i"
+else
+  opt="-d"
+fi
+opt="${opt}t"
+
+
+podman run "${opt}" --rm \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    --userns keep-id \
+    -v /run:/run \
+    -v /usr/share/fonts:/usr/share/fonts \
+    -v /sys:/sys \
+    -v /home/ryan:/home/ryan \
+    -v /proc:/proc \
+    -v /dev/shm:/dev/shm \
+    -v /tmp:/tmp \
+    --security-opt label=type:container_runtime_t \
+    -e "DISPLAY"  rstudio
