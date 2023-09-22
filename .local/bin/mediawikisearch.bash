97f0979ab410d9662e69547c89f7496e365c7bb5
Ryan G <exogenesis@protonmail.com>
2023-09-22 12:34:23 +1000

    fix mediawiki sqlite script

--- .local/bin/mediawikisearch.bash
+++ .local/bin/mediawikisearch.bash
@@ -1,27 +1,37 @@
-#!/bin/bash
-# see  the working directory ~/Studies/programming/mediawiki/fzfTitle/sk_mediawiki.bash
+#!/bin/sh
 
-SERVER_DIR="/home/ryan/Applications/Docker/"
-DB="${SERVER_DIR}mediawiki/sqlite_db/my_wiki.sqlite"
-IP="vale"
+IP="vidar"
 PORT="8076"
+BROWSER="firefox"
+DB="/home/ryan/Notes/mediawiki/sqlite_db/my_wiki.sqlite"
 
-# Query the user for a page name from wiki (and show preview with bat)
-page_name="$(
-		sqlite3 "${DB}" 'SELECT page_title FROM page;' |\
-			sk --ansi --preview "
-						sqlite3 "${DB}" '
-								SELECT old_text FROM text WHERE old_id is (
-									 SELECT content_id FROM content where content_sha1 is (
-										 SELECT rev_sha1 FROM revision WHERE rev_id is (
-											 SELECT page_latest FROM page WHERE page_title is \"{}\"
-											 )
-										 )
-									);
-								' | bat  --color=always -l org
-					    "
-           )"
+sqlite_content_from_title() {
+        printf "
+    -- Commented Columns are helpful for debugging
+    SELECT
+    --  p.page_title AS title
+        t.old_text AS text
+    --	p.page_namespace AS namespace
+    --  p.page_id AS id,
+    --  p.page_latest AS latest,
+    --  r.rev_sha1 AS revision_sha1,
+    --  c.content_id AS content_id,
+    FROM page AS p
+    INNER JOIN revision AS r ON p.page_latest = r.rev_id
+    INNER JOIN content AS c ON c.content_sha1 = r.rev_sha1
+    INNER JOIN text AS t ON t.old_id = c.content_id
+    WHERE p.page_namespace = 0
+    AND p.page_title = '%s'\n" "${1}"
+}
+
+get_page() {
+ sqlite3 "${DB}" 'SELECT page_title FROM page;' |\
+     sk --ansi --preview "sqlite3 ${DB} \"$(sqlite_content_from_title \"{}\")\" | bat --color=always -l org"
+}
 
 
-firefox "http://${IP}:${PORT}/index.php/${page_name}" & disown
+get_url() {
+    printf "http://${IP}:${PORT}/index.php/$(get_page)"
+}
 
+"${BROWSER}" "$(get_url)"
