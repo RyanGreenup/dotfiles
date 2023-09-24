04c74ca338bf0e98c556554b734ebd47c1f7b933
ryan <exogenesis@protonmail.com>
2023-09-24 22:18:31 +1000

    [ranger] configure for ansible

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -8,5 +8,6 @@ case "$1" in
     *.pdf) pdftotext "$1" -;;
     *.md) bat --color=always "$1" -;;
     *.markdown) bat --color=always "$1" -;;
+    *.ansible*) bat --color=always --language=yaml "$1" -;;
     *) highlight -O ansi "$1";;
 esac
