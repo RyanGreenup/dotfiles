9ccf6a93b5198f38d379b13c08482884455cde90
ryan <exogenesis@protonmail.com>
2023-11-24 20:29:52 +1100

    [org] enable weekly timegrid

--- .config/doom/personal_config/org-agenda/aesthetics.el
+++ .config/doom/personal_config/org-agenda/aesthetics.el
@@ -4,18 +4,18 @@
 ;; https://zzamboni.org/post/beautifying-org-mode-in-emacs/
 ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Face-Attributes.html
 (defun my-org-agenda-get-day-face-fn (date)
-"Return the face DATE should be displayed with."
+  "Return the face DATE should be displayed with."
   (let ((day-of-week (calendar-day-of-week date)))
     (cond
-      (
-        '(:height 160 :family "Fira Sans"
-;;          :box (:line-width (5 . 1) :color "red" :style "bold")
-          :underline (:color "indianred" :style line)
-          :inverse-video nil
-          :foreground "royalblue"
-          )
+     (
+      '(:height 160 :family "Fira Sans"
+        ;;          :box (:line-width (5 . 1) :color "red" :style "bold")
+        :underline (:color "indianred" :style line)
+        :inverse-video nil
+        :foreground "royalblue"
+        )
 
-        ))))
+      ))))
 
 (setq org-agenda-day-face-function 'my-org-agenda-get-day-face-fn)
 ;;; Misc Hooks etc
@@ -24,11 +24,11 @@
       :desc "Open Logseq Contents" "l c" #'org-logseq-toggle-contents-sidebar)
 (after! org
   (setq
-        org-agenda-span 7
-        ;; org-agenda-start-day "-3d"
-        org-agenda-start-on-weekday 1 ; 0 is sunday, 1 is monday ...
-        calendar-week-start-day     1 ; What the first day of subsequent agenda views is
-        org-agenda-start-on-weekday nil)
+   org-agenda-span 7
+   ;; org-agenda-start-day "-3d"
+   org-agenda-start-on-weekday 1 ; 0 is sunday, 1 is monday ...
+   calendar-week-start-day     1 ; What the first day of subsequent agenda views is
+   org-agenda-start-on-weekday nil)
   (setq org-tags-column 80))
 
 
@@ -38,3 +38,13 @@
 ;;          "......" "----------------"))
 ;; See also
 ;; (require 'calfw-org)
+
+
+;; Display the Time grid for every day of the week
+;; Change weekly to daily for the original value
+;; Toggle in Agenda with G, v g or M-x org-agenda-toggle-time-grid
+;; [[help:org-agenda-time-grid]]
+(setq org-agenda-time-grid
+      '((weekly today require-timed)
+        (800 1000 1200 1400 1600 1800 2000)
+        " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"))
