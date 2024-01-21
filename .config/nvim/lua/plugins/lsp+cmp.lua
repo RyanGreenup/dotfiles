a6bf88ff051a448cb6e6e6e0d39cc9f3e9a96795
ryan <ryan@base.vidar>
2024-01-22 10:52:16 +1100

    [nvim] format lsp.lua

--- .config/nvim/lua/plugins/lsp+cmp.lua
+++ .config/nvim/lua/plugins/lsp+cmp.lua
@@ -11,57 +11,57 @@
 local cmp = require 'cmp'
 
 cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-          -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-          -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-          require('snippy').expand_snippet(args.body) -- For `snippy` users.
-          -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        end,
-    },
-    mapping = {
-        -- https://teddit.net/r/neovim/comments/u7nsje/nvimcmp_completion_issue_cn_gives_basic_completion/
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs( -4), { 'i', 'c' }),
-        ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-        ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        ['<C-e>'] = cmp.mapping({
-            i = cmp.mapping.abort(),
-            c = cmp.mapping.close(),
-        }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    },
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'nvim_lsp' },
-        { name = 'otter' },
-        -- { name = 'vsnip' }, -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        { name = 'snippy' }, -- For snippy users.
-    }, {
-        { name = 'buffer' },
-        { name = 'path' },
-    })
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  mapping = {
+    -- https://teddit.net/r/neovim/comments/u7nsje/nvimcmp_completion_issue_cn_gives_basic_completion/
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'nvim_lsp' },
+    { name = 'otter' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  })
 })
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
-    sources = {
-        { name = 'buffer' }
-    }
+  sources = {
+    { name = 'buffer' }
+  }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
 })
 
 -- Add additional capabilities supported by nvim-cmp
@@ -96,7 +96,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wd',
-      '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
@@ -111,30 +111,30 @@ end
 -- map buffer local keybindings when the language server attaches
 
 require 'lspconfig'.lua_ls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
     }
+  }
 }
 
 local servers = {
-    'bashls', 'clangd', 'clojure_lsp', 'cmake', 'csharp_ls', 'dartls', 'dockerls',
-    'dotls', 'gopls', 'java_language_server', 'jsonls', 'lua_ls',
-    'kotlin_language_server', 'marksman', 'nimls', 'pyright', 'ruff_lsp', 'pylsp', 'quick_lint_js',
-    'r_language_server', 'racket_langserver', 'rust_analyzer', 'texlab',
-    'tsserver', 'sqlls', 'stylelint_lsp', 'vala_ls', 'vls', 'zls', 'ols',
-    'spectral', 'ansiblels', 'rome', 'jsonls', 'html', 'denols'
+  'bashls', 'clangd', 'clojure_lsp', 'cmake', 'csharp_ls', 'dartls', 'dockerls',
+  'dotls', 'gopls', 'java_language_server', 'jsonls', 'lua_ls',
+  'kotlin_language_server', 'marksman', 'nimls', 'pyright', 'ruff_lsp', 'pylsp', 'quick_lint_js',
+  'r_language_server', 'racket_langserver', 'rust_analyzer', 'texlab',
+  'tsserver', 'sqlls', 'stylelint_lsp', 'vala_ls', 'vls', 'zls', 'ols',
+  'spectral', 'ansiblels', 'rome', 'jsonls', 'html', 'denols'
 }
 
 local python_servers = {
-  'pylsp',        -- This is the main one, formatting requires it
-  'pyright',      -- This performs type checking and static analysis
-  'ruff_lsp'      -- This is like pyright but with less Microsoft
+  'pylsp',   -- This is the main one, formatting requires it
+  'pyright', -- This performs type checking and static analysis
+  'ruff_lsp' -- This is like pyright but with less Microsoft
 }
 
 for _, s in pairs(python_servers) do
@@ -144,8 +144,8 @@ end
 
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
-      on_attach = on_attach,
-      capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
   }
 end
 
@@ -246,27 +246,27 @@ end
 local M = {}
 
 M.icons = {
-    Class       = "",
-    Color       = "",
-    Constant    = "",
-    Constructor = "",
-    Enum        = " ",
-    EnumMember  = "",
-    Field       = "",
-    File        = "",
-    Folder      = "",
-    Function    = "",
-    Interface   = "ﰮ",
-    Keyword     = "",
-    Method      = "ƒ",
-    Module      = "",
-    Property    = "",
-    Snippet     = "﬌",
-    Struct      = "",
-    Text        = "",
-    Unit        = "",
-    Value       = "",
-    Variable    = "",
+  Class       = "",
+  Color       = "",
+  Constant    = "",
+  Constructor = "",
+  Enum        = " ",
+  EnumMember  = "",
+  Field       = "",
+  File        = "",
+  Folder      = "",
+  Function    = "",
+  Interface   = "ﰮ",
+  Keyword     = "",
+  Method      = "ƒ",
+  Module      = "",
+  Property    = "",
+  Snippet     = "﬌",
+  Struct      = "",
+  Text        = "",
+  Unit        = "",
+  Value       = "",
+  Variable    = "",
 }
 
 function M.setup()
