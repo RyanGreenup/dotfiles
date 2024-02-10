d0fb7cb544c4a16f9be42f15e28945f4bba29083
ryan <ryan@base.vidar>
2024-02-10 17:40:05 +1100

    [nvim] add neotree and re enable netrw

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -53,7 +53,16 @@ use({
 })
 
 
-
+use({
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  }
+})
 
 
 -- Tree Sitter
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -120,10 +120,10 @@ cmd [[
 -----------------------------------------------------------
 -- disable builtins plugins
 local disabled_built_ins = {
-  "netrw",
-  "netrwPlugin",
-  "netrwSettings",
-  "netrwFileHandlers",
+--  "netrw",
+--  "netrwPlugin",
+--  "netrwSettings",
+--  "netrwFileHandlers",
   "gzip",
   "zip",
   "zipPlugin",
