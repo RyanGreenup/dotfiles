ea6834f2554b954ca63cd34abf402f5327d575db
ryan <ryan@base.vidar>
2024-02-10 17:44:21 +1100

    [nvim] neotree now netrw hijack

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -58,10 +58,15 @@ use({
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",   -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-  }
+  },
+  config = function()
+    require("neo-tree").setup({
+      hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
+    })
+  end
 })
 
 
