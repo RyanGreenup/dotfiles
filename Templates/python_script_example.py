328cc8c2853a993a973f377248ad1094da4bc316
ryan <exogenesis@protonmail.com>
2024-02-28 07:04:03 +1100

    [template] improve

--- Templates/python_script_example.py
+++ Templates/python_script_example.py
@@ -8,6 +8,7 @@ This script accepts a string as a command-line argument and prints it out.
 
 # Import necessary standard Python libraries
 import os
+import subprocess
 
 # Import argparse for command line arguments
 import argparse
@@ -35,12 +36,38 @@ def parse_args():
     """
     Parse command line arguments
     """
-    parser = argparse.ArgumentParser(description="Display user-supplied string")
+    parser = argparse.ArgumentParser(
+        description="Display user-supplied string")
     parser.add_argument(
         "--argument", type=str, help="Enter a string you want to display"
     )
     args = parser.parse_args()
     return args
+
+
+# .............................................................................
+# Testing Example .............................................................
+# .............................................................................
+
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the result
+    """
+    return a + b
+
+
+def test_add_numbers():
+    """
+    Tests the add_numbers function
+    """
+    assert add_numbers(1, 2) == 3
+    assert add_numbers(0, 0) == 0
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(-1, -1) == -2
+
+# .............................................................................
+# Main ........................................................................
+# .............................................................................
 
 
 if __name__ == "__main__":
