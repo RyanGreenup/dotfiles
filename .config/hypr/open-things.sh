c2a8c9e18e18297d0ada624e27c1cfbbf9b63c2e
Shaira Viaje <shaira.viaje@gmail.com>
2024-03-10 18:35:22 +1100

    [hyprland] my stuff is still in flatpak atm

--- .config/hypr/open-things.sh
+++ .config/hypr/open-things.sh
@@ -2,7 +2,8 @@
 
 notes() {
     firefox-bin --profile ~/.mozilla/firefox/webapp http://localhost:3818/linux/too-many-open-files & disown
-    $HOME/Applications/AppImages/Obsidian-1.5.8.AppImage --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto --disable-gpu & disown
+    ## $HOME/Applications/AppImages/Obsidian-1.5.8.AppImage --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto --disable-gpu & disown
+    flatpak run md.obsidian.Obsidian & disown
 }
 
 agenda() {
@@ -12,8 +13,10 @@ agenda() {
 }
 
 messages() {
-    /usr/bin/distrobox-enter  -n containerized_apps-signal-desktop -- /bin/sh -l -c  /opt/Signal/signal-desktop   --no-sandbox & disown
-    /usr/bin/distrobox-enter  -n containerized_apps-signal-desktop -- /bin/sh -l -c  /opt/Element/element-desktop              & disown
+    flatpak run org.signal.Signal & disown
+    flatpan run im.riot.Riot & disown
+    ## /usr/bin/distrobox-enter  -n containerized_apps-signal-desktop -- /bin/sh -l -c  /opt/Signal/signal-desktop   --no-sandbox & disown
+    ## /usr/bin/distrobox-enter  -n containerized_apps-signal-desktop -- /bin/sh -l -c  /opt/Element/element-desktop              & disown
 }
 
 options() {
