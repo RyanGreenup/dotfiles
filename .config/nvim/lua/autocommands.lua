c7a41d51ab09632acc04fa46389f698b0e06893a
Shaira Viaje <shaira.viaje@gmail.com>
2024-04-09 16:58:43 +1000

    [nvim

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -34,6 +34,8 @@ vim.cmd [[
   autocmd FileType zig map <buffer>  <F2> :w<CR>:exec      '!zig run' shellescape(@%, 1)<CR>
   autocmd FileType zig imap <buffer>  <F2> <esc>:w<CR>:exec '!zig run' shellescape(@%, 1)<CR>
 
+  autocmd FileType markdown set foldmethod=expr
+  autocmd FileType markdown set foldexpr=nvim_treesitter#foldexpr()
 
 ]]
 
