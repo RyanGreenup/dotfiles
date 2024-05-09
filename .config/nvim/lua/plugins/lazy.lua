3f3345768a33ddedc9ec9a287679aa0848b7219c
ryan <exogenesis@protonmail.com>
2024-05-09 10:05:51 +1000

    [nvim] change arrow binding

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -215,7 +215,7 @@ use {
   "otavioschwanck/arrow.nvim",
   opts = {
     show_icons = true,
-    leader_key = ';',            -- Recommended to be a single key
+    leader_key = '\\',            -- Recommended to be a single key
     buffer_leader_key = '<F12>', -- Per Buffer Mappings
   }
 }
