693db563052e85cadb0dc066b3f4b4f66ee61988
ryan <exogenesis@protonmail.com>
2024-05-23 18:54:37 +1000

    [nvim] simplified autocommands

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,95 +1,6 @@
-local cmd = vim.cmd -- execute Vim commands
-
 -- Create Groups --------------------------------------------------------------
 local python_group = vim.api.nvim_create_augroup("Python", { clear = true })
 local markdown_group = vim.api.nvim_create_augroup("Markdown", { clear = true })
-
--- Create Functions
-function file_autocmd_command(pattern, group, command, desc)
-  vim.api.nvim_create_autocmd(
-    { "BufNewFile", "BufRead", "FileType" },
-    {
-      pattern = pattern,
-      group = group,
-      command = command,
-      desc = desc,
-    })
-end
-
-function file_autocmd_function(event, pattern, group, callback_function)
-  vim.api.nvim_create_autocmd(event,
-    {
-      pattern = pattern,
-      group = group,
-      callback = callback_function
-    })
-end
-
--- Create Factories -----------------------------------------------------------
-function file_autocmd_command_factory(pattern, group)
-  return function(command, desc)
-    file_autocmd_command(pattern, group, command, desc)
-  end
-end
-
-file_autocmd_function_factory = function(pattern, group)
-  return function(event, callback_function)
-    file_autocmd_function(event, pattern, group, callback_function)
-  end
-end
-
-
-
-
--- Python Autocommands --------------------------------------------------------
-local py_pattern = "*.py"
--- Create Helpers
-file_autocmd_command_py = file_autocmd_command_factory('*.py', python_group)
-file_autocmd_function_py = file_autocmd_function_factory('*.py', python_group)
--- Autocmd Commands
-file_autocmd_command_py('set cindent', 'Allow Indenting Comments and Code in Python')
-vim.api.nvim_create_autocmd(
-  { "BufNewFile" },
-  {
-    pattern = py_pattern,
-    group = python_group,
-    command = '0r ~/Templates/python_script.py',
-    desc = "Load Python Template",
-  })
--- Autocmd Callback Functions
--- Python Autocommands --------------------------------------------------------
--- Create Helpers
-file_autocmd_command_md = file_autocmd_command_factory('*.md', markdown_group)
-file_autocmd_function_md = file_autocmd_function_factory('*.md', markdown_group)
--- Autocmd Commands
-file_autocmd_command_md('set foldexpr=nvim_treesitter#foldexpr()', 'Set Fold Expression to Treesitter')
-file_autocmd_command_md('lua print("Hello")', 'Print Hello in Lua')
-file_autocmd_command_md('set foldmethod=expr', 'Set Fold Method to Expression')
-file_autocmd_command_py('set cindent', 'Allow Indenting Comments and Code in code blocks')
--- [^6158294]
-file_autocmd_command_md(
-  ':nmap <C-CR> "<Esc>:e <cfile><CR>" <CR>',
-  'Create a New Markdown File from bracketed link')
--- Autocmd Callback Functions
-
--- Ansible Autocommands -------------------------------------------------------
--- NOTE This breaks the mnemonic above, because there's multiple exts
-local exts = { "*yaml.ansible", "*ansible.yaml", "*playbook.yaml" }
-local commands = {
-  "set filetype=yaml",
-  "LspStart ansiblels",
-  [[ nmap <F2> :w<CR>:!ansible-playbook "%"<CR> ]],
-  [[ nmap <F3> :!neofetch ]],
-  "set foldmethod=indent",
-}
-
-for _, ext in ipairs(exts) do
-  for _, c in ipairs(commands) do
-    local s = "autocmd BufNewFile,BufRead " .. ext .. " " .. c
-    vim.cmd(s)
-  end
-end
-
 
 
 -- Keymaps --------------------------------------------------------------------
@@ -126,22 +37,14 @@ for _, value in ipairs(run_file) do
   end
 end
 
--- [[ References
--- [^6158294]: https://stackoverflow.com/questions/6158294/how-to-create-and-open-for-editing-a-nonexistent-file-whose-path-is-under-the-cu
--- ]]
 
-
--- TODO  Clean up this file to have a better abstraction for FileType, BufNewFile, BufRead etc.
--- vim.cmd [[
---
---   autocmd FileType markdown set foldexpr=nvim_treesitter#foldexpr()
---   autocmd FileType markdown set foldmethod=expr
--- ]]
+-- Markdown
 
 vim.api.nvim_create_autocmd(
   { "FileType" },
   {
     pattern = "markdown",
+    group = markdown_group,
     command = "set foldexpr=nvim_treesitter#foldexpr()",
     desc = "Set Fold Expression to Treesitter",
 
@@ -151,6 +54,7 @@ vim.api.nvim_create_autocmd(
   { "FileType" },
   {
     pattern = "markdown",
+    group = markdown_group,
     command = "set foldmethod=expr",
     desc = "Set Fold Method to Expression",
 
@@ -160,10 +64,13 @@ vim.api.nvim_create_autocmd(
   { "FileType" },
   {
     pattern = "markdown",
+    group = markdown_group,
     command = "set cindent",
     desc = "Set Fold Method to Expression",
 
   })
+
+-- Python
 
 vim.api.nvim_create_autocmd(
   { "FileType" },
@@ -173,3 +80,36 @@ vim.api.nvim_create_autocmd(
     desc = "Set Fold Expression to Treesitter",
 
   })
+
+
+vim.api.nvim_create_autocmd(
+  { "BufNewFile" },
+  {
+    pattern = "python",
+    group = python_group,
+    command = '0r ~/Templates/python_script.py',
+    desc = "Load Python Template",
+  })
+
+
+-- Ansible Autocommands -------------------------------------------------------
+-- NOTE This breaks the mnemonic above, because there's multiple exts
+local exts = { "*yaml.ansible", "*ansible.yaml", "*playbook.yaml" }
+local commands = {
+  "set filetype=yaml",
+  "LspStart ansiblels",
+  [[ nmap <F2> :w<CR>:!ansible-playbook "%"<CR> ]],
+  [[ nmap <F3> :!neofetch ]],
+  "set foldmethod=indent",
+}
+
+for _, ext in ipairs(exts) do
+  for _, c in ipairs(commands) do
+    local s = "autocmd BufNewFile,BufRead " .. ext .. " " .. c
+    vim.cmd(s)
+  end
+end
+
+-- [[ References
+-- [^6158294]: https://stackoverflow.com/questions/6158294/how-to-create-and-open-for-editing-a-nonexistent-file-whose-path-is-under-the-cu
+-- ]]
