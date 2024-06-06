6e0b4ee4441ac08292d18b510c9eb7270279993c
ryan <exogenesis@protonmail.com>
2024-06-07 07:35:10 +1000

    [bump] fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,33 @@
+# Set PATH
+set PATH /usr/local/bin/ $PATH
+set PATH $HOME/.local/bin $PATH
+set PATH $HOME/bin $PATH
+set PATH $HOME/.cargo/bin $PATH
+set PATH $HOME/.gem/ruby/2.7.0/bin/ $PATH
+set PATH $HOME/.local/share/gem/ruby/3.2.0/bin $PATH
+set PATH $HOME/go/bin $PATH
+set PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
+set PATH $PATH /usr/lib/rstudio
+
+# Add AppImages
+set PATH $PATH $HOME/Applications/AppImages/bin/
+
+# Add Flatpak
+set PATH $PATH /var/lib/flatpak/exports/bin/
+set XDG_DATA_DIRS $XDG_DATA_DIRS:/var/lib/flatpak/exports/share/
+
+# Add Flatpak
+set PATH $PATH $HOME/.nix-profile/bin/
+
+set PATH $HOME/.local/share/nvim/mason/bin/ $PATH
+
+export QT_XCB_GL_INTEGRATION=none
+# <https://github.com/NixOS/nixpkgs/issues/169630>
+
 # Set Default Editor to Emacs
 # set VISUAL 'emacs -nw --eval "(add-hook \'emacs-startup-hook #\'sh-mode)"'
 export VISUAL=nvim
 export EDITOR=nvim
-
-function __get_distro
-    cat /etc/os-release | grep -e '^ID=' | cut -d '=' -f 2 | sed 's/"//g'
-end
-
-
-if status is-interactive
-    if command -v python 1>/dev/null 2>&1
-        set distro (__get_distro)
-        set venv_dir /usr/local/venv/default/$distro
-        # Check if the directory exists
-        if test -d $venv_dir
-            source $venv_dir/bin/activate.fish
-
-        else
-            echo "No virtual environment for $distro"
-            echo "Create one with:"
-            echo ""
-            echo "    python -m venv $venv_dir"
-            echo ""
-            echo "chgrp (id -u) " $venv_dir
-            echo "chmod 775 "     $venv_dir
-        end
-    end
-end
 
 export SVDIR=$HOME/.local/service
 
@@ -38,7 +39,7 @@ if test -d /opt/libtorch
 end
 
 function v --wraps=nvim --description 'alias v=nvim'
-  nvim $argv;
+    nvim $argv
 end
 
 function f --wraps='cd ; exa -RGL 3' --description 'alias f=cd; exa -RGL 3'
@@ -60,13 +61,22 @@ function ls! --wraps='ls -ultrah' --description 'alias ls!=ls -ultrah'
 end
 
 ## Easier Xclip
-function x --wraps='xclip -selection clipboard' --description 'Alias for xclip'
-    xclip -selection clipboard $argv
+function xp
+    if test -z $WAYLAND_DISPLAY
+        xclip -selection clipboard -out
+    else
+        wl-paste
+    end
 end
 
-function bn
-    bulk_rename.py
+function x
+    if test -z $WAYLAND_DISPLAY
+        xclip -selection clipboard
+    else
+        wl-copy
+    end
 end
+
 
 ## Easy weather
 function wtr
@@ -81,8 +91,8 @@ function wtr
     if $have_weather
         bat /tmp/weather.txt
     else
-        curl v2.wttr.in > /tmp/weather.txt && set have_weather true
-        curl wttr.in >> /tmp/weather.txt && set have_weather true
+        curl v2.wttr.in >/tmp/weather.txt && set have_weather true
+        curl wttr.in >>/tmp/weather.txt && set have_weather true
     end
 
     if ! $have_weather
@@ -100,8 +110,8 @@ function k!
 end
 
 function open_dokuwiki_clipboard
-   set file \
-       (xclip -sel clip -o |\
+    set file \
+        (xclip -sel clip -o |\
           awk -F '/' '{print $NF}' |\
           awk -F '=' '{print $NF}' |\
           sed 's#:#/#' |\
@@ -117,8 +127,8 @@ end
 # Toggle Alacritty theme
 function tt
     # If the colors: line is found, use sed to change it to dark or light
-    grep  'colors: \*light' ~/.config/alacritty/alacritty.yml && sed -i  's!colors:\ \*light!colors: *dark!' ~/.config/alacritty/alacritty.yml && return 0
-    grep  'colors: \*dark'  ~/.config/alacritty/alacritty.yml && sed -i  's!colors:\ \*dark!colors: *light!' ~/.config/alacritty/alacritty.yml && return 0
+    grep 'colors: \*light' ~/.config/alacritty/alacritty.yml && sed -i 's!colors:\ \*light!colors: *dark!' ~/.config/alacritty/alacritty.yml && return 0
+    grep 'colors: \*dark' ~/.config/alacritty/alacritty.yml && sed -i 's!colors:\ \*dark!colors: *light!' ~/.config/alacritty/alacritty.yml && return 0
 end
 
 function ws
@@ -134,13 +144,13 @@ function zj
 end
 
 if status is-interactive
-   if command -v broot 1> /dev/null 2>&1
+    if command -v broot 1>/dev/null 2>&1
         broot --print-shell-function fish | source
     end
 end
 
 if status is-interactive
-   if command -v zoxide 1> /dev/null 2>&1
+    if command -v zoxide 1>/dev/null 2>&1
         zoxide init fish | source
     end
 end
@@ -163,14 +173,14 @@ end
 # * Notetaking Stuff ...........................................................
 # ..............................................................................
 set __agenda_dir $HOME/Agenda
-set __notes_dir $HOME/Notes
+set __notes_dir $HOME/Notes/slipbox
 set __notes_old $HOME/Sync/Notes
-set __notes_dw  /srv/http/dokuwiki/data
+set __notes_dw /srv/http/dokuwiki/data
 set __note_taking_dirs $__notes_dir $__notes_old $__notes_dw
 
 # git
 function __try_run
-    command -v $argv[1] > /dev/null 2>&1 && $argv[1]
+    command -v $argv[1] >/dev/null 2>&1 && $argv[1]
 end
 
 function __git_helper
@@ -187,9 +197,9 @@ end
 
 # open non empty arguments in EDITOR
 function _private_open
-  if [ ! (count $argv) -eq 0 ]
-      $EDITOR $argv
-  end
+    if [ ! (count $argv) -eq 0 ]
+        $EDITOR $argv
+    end
 end
 
 # ** Searching .................................................................
@@ -197,13 +207,12 @@ function _private_search
     set notes_dir $argv
     cd $notes_dir
 
-        sk -m -i -c "note_taking search -d "$notes_dir"  {}"        \
-            --bind pgup:preview-page-up,pgdn:preview-page-down      \
-            --preview "bat --style grid --color=always              \
+    sk -m -i -c "note_taking search -d "$notes_dir"  {}" \
+        --bind pgup:preview-page-up,pgdn:preview-page-down \
+        --preview "bat --style grid --color=always              \
                             --terminal-width 80 $notes_dir/{+}      \
                             --italic-text=always                    \
-                            --decorations=always"                |  \
-        sed "s#^#$notes_dir/#"
+                            --decorations=always" | sed "s#^#$notes_dir/#"
 end
 
 
@@ -212,8 +221,15 @@ function ns
     _private_open (_private_search $__notes_dir)
 end
 
+
+
+
 function nl
-    $HOME/.config/fish/note_aliases.py --nl --notes_dir $__notes_dir
+    notes.make_link.py --nl --notes_dir $__notes_dir
+end
+
+function nsem
+    note_taking sem -d $__notes_dir
 end
 
 # *** Search ALL notes
@@ -229,10 +245,10 @@ end
 # *** Reindex notes
 
 function nR
-  for dir in $__note_taking_dirs
-    echo $dir
-    note_taking reindex -d $dir
-  end
+    for dir in $__note_taking_dirs
+        echo $dir
+        note_taking reindex -d $dir
+    end
 end
 
 function nr
@@ -242,7 +258,7 @@ end
 # ** Finding ......................................................................
 ## I could have used `note_taking fzf` but skim and bat is prettier
 function _private_finding
-   # use ls -t to sort by time (default is modification time)
+    # use ls -t to sort by time (default is modification time)
     ls -t (fd -t f '\.org$|\.md$|\.txt$' $argv) |
         sk --ansi -m -c 'rg -l -t markdown -t org -t txt --ignore-case "{}"' \
             --preview "bat --style snip {} 2> /dev/null --color=always" \
@@ -251,17 +267,19 @@ end
 
 # *** Find main notes
 function nf --description 'Find Notes'
-    _private_open (_private_finding $__notes_dir)
+    # _private_open (_private_finding $__notes_dir)
+   ~/.local/bin/notes.find.py --editor nvim
 end
 
 # *** Find ALL notes
 function nF
     # Find the notes and open if not cancelled
-    _private_open (_private_finding $__note_taking_dirs)
+    # _private_open (_private_finding $__note_taking_dirs)
+    ~/.local/bin/notes.find.py
 end
 
 function nfm
-  ~/.local/bin/mediawikisearch.bash
+    ~/.local/bin/mediawikisearch.bash
 end
 
 # function nn
@@ -273,7 +291,7 @@ function nno
     set title (read)
     echo $notes_dir
     set file (readlink -f "$__notes_dir/pages/$title.org") # use readlink to clean path
-    echo "# $title" >> $file
+    echo "# $title" >>$file
     emacs $file
 end
 
@@ -282,11 +300,11 @@ function nnm
     set title (read)
     echo $notes_dir
     set file (readlink -f "$__notes_dir/pages/$title.md") # use readlink to clean path
-    echo "# $title" >> $file
+    echo "# $title" >>$file
     $EDITOR $file
 end
 
-function nn
+function nn_old
     # note_taking new -d "$__notes_dir"
 
     # TODO wrap this into the go program
@@ -303,10 +321,15 @@ function nn
     if test -f $filename
         nvim $filename
     else
-        echo $title | sed 's!^!# !' >> $HOME/Notes/slipbox/$filename
+        echo $title | sed 's!^!# !' >>$HOME/Notes/slipbox/$filename
     end
     nvim $filename
     rm $file
+end
+
+function nn
+    # set root (fd '\.md$' ~/Notes/slipbox/ | rev | cut -d '.' -f 3- | rev | sort -u | sed "s#$HOME/Notes/slipbox/##" | fzf)
+    ~/.local/bin/notes.new.py
 end
 
 # ..............................................................................
@@ -319,11 +342,8 @@ function get_os
 end
 
 function void_query_packages
-    xbps-query -Rs '' |\
-        rg -o '[\w-]+-'  |\
-        sed 's!-$!!'     |\
-        fzf --multi --preview \
-            'xbps-query -S {} || echo No Info Available'
+    xbps-query -Rs '' | rg -o '[\w-]+-' | sed 's!-$!!' | fzf --multi --preview \
+        'xbps-query -S {} || echo No Info Available'
 end
 
 function arch_pz
@@ -332,16 +352,16 @@ end
 
 function pz --description 'Fuzzy Find to preview and install packages'
     switch (get_os)
-    case 'void'
-        if set packages (void_query_packages)
-            doas xbps-install $packages
-        end
-    case 'arch'
-        arch_pz
-    case 'endeavouros'
-        arch_pz
-    case "*"
-        echo "Operating System $os is not configured"
+        case void
+            if set packages (void_query_packages)
+                doas xbps-install $packages
+            end
+        case arch
+            arch_pz
+        case endeavouros
+            arch_pz
+        case "*"
+            echo "Operating System $os is not configured"
     end
 end
 
@@ -365,36 +385,36 @@ end
 
 function start_podman_containers
     if status is-interactive
-        if command -v podman-compose > /dev/null 2>&1
-                for yaml in (ls ~/Applications/Containers/user/vidar/**/docker-compose.yml)
-                        # get the container name
-                        set name (basename (dirname $yaml))
-                        # Not necessary but this means I can use the same snippet to restart
-                        podman-compose -f $yaml down
-                        # Start the containers
-                        podman-compose -f $yaml up -d \
-                                && printf '\n\n SUCCESS -- %s \n\n'  $name \
-                                || printf '\n\n FAILURE  -- %s \n\n' $name
-                end
+        if command -v podman-compose >/dev/null 2>&1
+            for yaml in (ls ~/Applications/Containers/user/vidar/**/docker-compose.yml)
+                # get the container name
+                set name (basename (dirname $yaml))
+                # Not necessary but this means I can use the same snippet to restart
+                podman-compose -f $yaml down
+                # Start the containers
+                podman-compose -f $yaml up -d \
+                    && printf '\n\n SUCCESS -- %s \n\n' $name \
+                    || printf '\n\n FAILURE  -- %s \n\n' $name
+            end
         else
-                echo "podman-compose is missing, try:"
-                echo ""
-                echo "    ```"
-                echo "    pipx install podman-compose"
-                echo "    ```"
+            echo "podman-compose is missing, try:"
+            echo ""
+            echo "    ```"
+            echo "    pipx install podman-compose"
+            echo "    ```"
         end
-     end
+    end
 end
 
 if status is-interactive
-    if command -v starship > /dev/null 2>&1
+    if command -v starship >/dev/null 2>&1
         starship init fish --print-full-init | source
     end
 end
 
 # Create keybindings
 function fish_user_key_bindings
-	fzf_key_bindings
+    fzf_key_bindings
 end
 bind \en '
     set tmp (mktemp)       && \
@@ -415,29 +435,97 @@ bind \co '
     rm $tmp
     commandline -f repaint'
 
-# Set PATH
-set PATH /usr/local/bin/            $PATH
-set PATH $HOME/.local/bin           $PATH
-set PATH $HOME/bin                  $PATH
-set PATH $HOME/.cargo/bin           $PATH
-set PATH $HOME/.juliaup/bin         $PATH
-set PATH $HOME/.gem/ruby/2.7.0/bin/ $PATH
-set PATH $HOME/.local/share/gem/ruby/3.2.0/bin $PATH
-set PATH $HOME/go/bin               $PATH
-set PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
-set PATH $PATH /usr/lib/rstudio
 
-# Add AppImages
-set PATH $PATH $HOME/Applications/AppImages/bin/
 
-# Add Flatpak
-set PATH $PATH /var/lib/flatpak/exports/bin/
-set XDG_DATA_DIRS $XDG_DATA_DIRS:/var/lib/flatpak/exports/share/
 
-# Add Flatpak
-set PATH $PATH $HOME/.nix-profile/bin/
 
-set PATH $HOME/.local/share/nvim/mason/bin/  $PATH
+if status is-interactive
+    if command -v hoard 1>/dev/null 2>&1
+        hoard shell-config -s fish | source
+    end
+end
 
-export QT_XCB_GL_INTEGRATION=none
-# <https://github.com/NixOS/nixpkgs/issues/169630>
+
+# TODO this should be python
+function __get_distro
+    cat /etc/os-release | grep -e '^ID=' | cut -d '=' -f 2 | sed 's/"//g'
+end
+
+# TODO check hostname for distrobox
+#      Or maybe just check os-release?
+if status is-interactive
+    if command -v python 1>/dev/null 2>&1
+        set distro (__get_distro)
+        set venv_dir /usr/local/venv/default/$distro
+# Check if the directory exists
+        if test -d $venv_dir
+            source $venv_dir/bin/activate.fish
+            echo "Activated $venv_dir"
+        else
+            echo "No virtual environment for $distro"
+            echo "Create one with:"
+            echo ""
+            echo "    python -m venv $venv_dir"
+            echo ""
+            echo "chgrp (id -u) " $venv_dir
+            echo "chmod 775 "     $venv_dir
+        end
+    end
+end
+
+# Everything should simply be alias --------------------------------------------
+set __scripts_dir $HOME/.local/scripts/python
+set __note_scripts $__scripts_dir/notes
+function ns
+    ~/.local/scripts/python/notes/search.py search  '' --fzf
+end
+
+function nsl
+    ~/.local/scripts/python/notes/search__live.py
+end
+
+function nf
+    ~/.local/scripts/python/notes/find.py
+end
+
+function nl
+    ~/.local/scripts/python/notes/make_link.py
+end
+
+
+function nS
+    ~/.local/scripts/python/notes/search__semantic.py
+end
+
+function nj
+    ~/.local/scripts/python/notes/journals__open.py
+end
+
+function x
+    ~/.local/scripts/python/wm__clipboard.py copy
+end
+
+function xp
+    ~/.local/scripts/python/wm__clipboard.py paste
+end
+
+function bn
+    ~/.local/scripts/python/os__utils__bulk_rename.py
+end
+
+function nn
+    ~/.local/scripts/python/notes/new.py
+end
+
+
+function nR
+    ~/.local/scripts/python/notes/search.py reindex
+end
+
+function nd
+    ~/.local/scripts/python/notes/dashboard.py --editor="codium"
+end
+
+function ai
+    python /home/ryan/.local/scripts/python/query-ollama.py data/prompts/write-article.md  data/prompts/write-article_system.md "$argv"
+end
