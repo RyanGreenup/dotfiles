11783474eeac3b4809f6c92eacc07ca8a35dd7ce
ryan <exogenesis@protonmail.com>
2024-07-06 13:38:02 +1000

    [nvim] add signature help

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -42,6 +42,7 @@ use({
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
     {
       'dcampos/cmp-snippy',
       dependencies = { {
