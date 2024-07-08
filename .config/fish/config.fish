009fb1745debfdf828d107c3a0b643298a7f1e68
ryan <exogenesis@protonmail.com>
2024-07-08 14:24:32 +1000

    [fish] yazi yy binding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -159,6 +159,15 @@ function lfcd
     end
 end
 
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 # ..............................................................................
 # * Notetaking Stuff ...........................................................
 # ..............................................................................
