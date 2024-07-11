ab774a6cb20fd4472549a0f7d8629201667dbdb6
ryan <exogenesis@protonmail.com>
2024-07-11 03:54:39 +0000

    [qutebrowser] edit dokuwiki

--- .config/qutebrowser/userscripts/edit_dw.sh
+++ .config/qutebrowser/userscripts/edit_dw.sh
@@ -1,17 +1,21 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 # editor='emacsclient --create-frame'
-editor='alacritty -e nvim'
+# editor='alacritty -e nvim'
+editor='neovide'
 
 # sd calls:
 #  1. remove #anchor-tags
 #  2. extract id
 #  3. replace ':' with '/'
 #  4. prend the file path
+# DOKU_BASE="$HOME/Notes/dokuwiki/data/pages"
+DOKU_BASE="$HOME/Applications/Docker/dokuwiki/config/dokuwiki/data/pages/"
+# notify-send "Opening ${QUTE_URL}"
 file="$(echo "${QUTE_URL}" |\
      sd '.*id=(.*)#.*$' 'id=$1' |\
      sd '.*id=(.*)' '$1.txt' |\
      sd ':' '/' | tr -d '\n' |\
-     sd '^' "$HOME/Notes/dokuwiki/config/dokuwiki/data/pages/")"
+     sd '^' "${DOKU_BASE}")"
 
 ${editor} "${file}"
