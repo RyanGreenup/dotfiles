a59a4508131200819bfa4c153df5fb46ac85993d
ryan <exogenesis@protonmail.com>
2024-07-11 01:55:23 +0000

    [nvim] yazi

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -549,6 +549,38 @@ use {
 }
 
 
+use {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  opts = {
+    yazi_floating_window_winblend = 0.1,
+  },
+  keys = {
+    -- 👇 in this section, choose your own keymappings!
+    {
+      "<leader>-",
+      function()
+        require("yazi").yazi()
+      end,
+      desc = "Open the file manager",
+    },
+    {
+      -- Open in the current working directory
+      "<leader>cw",
+      function()
+        require("yazi").yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "Open the file manager in nvim's working directory",
+    },
+  },
+  ---@type YaziConfig
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+  },
+}
+
+
 --------------------------------------------------------------------------------
 -- Finally Call the plugin Manager ---------------------------------------------
 --------------------------------------------------------------------------------
