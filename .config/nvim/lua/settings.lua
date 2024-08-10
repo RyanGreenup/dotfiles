ab4313a2bed1f62e61a055e6d80e07ab476d6f64
ryan <exogenesis@protonmail.com>
2024-08-10 19:19:40 +1000

    formatted settings

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -8,21 +8,21 @@
 
 
 local map = vim.api.nvim_set_keymap -- set global keymap
-local cmd = vim.cmd -- execute Vim commands
-local exec = vim.api.nvim_exec -- execute Vimscript
-local fn = vim.fn -- call Vim functions
-local g = vim.g -- global variables
-local opt = vim.opt -- global/buffer/windows-scoped options
+local cmd = vim.cmd                 -- execute Vim commands
+local exec = vim.api.nvim_exec      -- execute Vimscript
+local fn = vim.fn                   -- call Vim functions
+local g = vim.g                     -- global variables
+local opt = vim.opt                 -- global/buffer/windows-scoped options
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
 
-g.mapleader = ' ' -- change leader to a comma
+g.mapleader = ' '             -- change leader to a comma
 vim.api.nvim_set_keymap('i', '<M-Space>', '<Esc><leader>', { noremap = true })
-opt.mouse = 'a' -- enable mouse support
+opt.mouse = 'a'               -- enable mouse support
 opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
-opt.swapfile = false -- don't use swapfile
+opt.swapfile = false          -- don't use swapfile
 cmd [[
       imap jk <Esc>              " Get jk for Esc
       let g:markdown_folding=1   " Default Markdown Folding
@@ -31,25 +31,25 @@ cmd [[
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-opt.number = true -- show line number
-opt.showmatch = true -- highlight matching parenthesis
-opt.foldlevelstart = 99 -- Open everything up at first
-opt.colorcolumn = '80' -- line lenght marker at 80 columns
-opt.splitright = true -- vertical split to the right
-opt.splitbelow = true -- horizontal split to the bottom
-opt.ignorecase = true -- ignore case letters when search
-opt.smartcase = true -- ignore lowercase for the whole pattern
-opt.linebreak = true -- wrap on word boundary
-opt.autoread = true -- Automatically reload files
-opt.relativenumber = true -- Relative numbers
+opt.number = true          -- show line number
+opt.showmatch = true       -- highlight matching parenthesis
+opt.foldlevelstart = 99    -- Open everything up at first
+opt.colorcolumn = '80'     -- line lenght marker at 80 columns
+opt.splitright = true      -- vertical split to the right
+opt.splitbelow = true      -- horizontal split to the bottom
+opt.ignorecase = true      -- ignore case letters when search
+opt.smartcase = true       -- ignore lowercase for the whole pattern
+opt.linebreak = true       -- wrap on word boundary
+opt.autoread = true        -- Automatically reload files
+opt.relativenumber = true  -- Relative numbers
 cmd [[ set modelineexpr ]] -- See vim modeline vulnerability 2019
 
 -- Folding
 opt.foldmethod = "expr" -- fold based on Treesitter Expression
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-opt.foldtext = "v:lua.vim.treesitter.foldtext()"
-opt.foldlevel = 99 -- Start with everything unfolded
-opt.fillchars:append({fold = " "}) -- Don't show fold icons
+-- opt.foldtext = "v:lua.vim.treesitter.foldtext()" -- This is null if undefined, use autocommand if needed
+opt.foldlevel = 99                   -- Start with everything unfolded
+opt.fillchars:append({ fold = " " }) -- Don't show fold icons
 
 -- remove whitespace on save
 cmd [[au BufWritePre * :%s/\s\+$//e]]
@@ -67,10 +67,10 @@ exec([[
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-opt.hidden = true -- enable background buffers
-opt.history = 100 -- remember n lines in history
+opt.hidden = true     -- enable background buffers
+opt.history = 100     -- remember n lines in history
 opt.lazyredraw = true -- faster scrolling
-opt.synmaxcol = 240 -- max column for syntax highlight
+opt.synmaxcol = 240   -- max column for syntax highlight
 
 -----------------------------------------------------------
 -- Colorscheme
@@ -81,9 +81,9 @@ cmd [[colorscheme rose-pine ]]
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = true -- use spaces instead of tabs
-opt.shiftwidth = 4 -- shift 4 spaces when tab
-opt.tabstop = 4 -- 1 tab == 4 spaces
+opt.expandtab = true   -- use spaces instead of tabs
+opt.shiftwidth = 4     -- shift 4 spaces when tab
+opt.tabstop = 4        -- 1 tab == 4 spaces
 opt.smartindent = true -- autoindent new lines
 
 -- don't auto commenting new lines
@@ -123,10 +123,10 @@ cmd [[
 -----------------------------------------------------------
 -- disable builtins plugins
 local disabled_built_ins = {
---  "netrw",
---  "netrwPlugin",
---  "netrwSettings",
---  "netrwFileHandlers",
+  --  "netrw",
+  --  "netrwPlugin",
+  --  "netrwSettings",
+  --  "netrwFileHandlers",
   "gzip",
   "zip",
   "zipPlugin",
@@ -163,12 +163,13 @@ function Line_to_esperanto()
   -- Read the current line as a string
   local cl = vim.api.nvim_get_current_line()
   -- Combine into a shell command
-  local command = "argos-translate -f en -t eo '"..cl.."'"
+  local command = "argos-translate -f en -t eo '" .. cl .. "'"
   -- Run the shell command and read the output as a string
   local out = io.popen(command):read()
   -- Replace the line with the translation
   vim.api.nvim_set_current_line(out)
 end
+
 map('n', '<M-r>', ':lua Line_to_esperanto() <CR>', { noremap = true })
 
 
@@ -198,71 +199,58 @@ if exists('g:neoray')
   set guifont=:12
 endif
 
-
-if exists('g:neovide')
-  let g:neovide_scale_factor=1.0
-  function! ChangeScaleFactor(delta)
-      let g:neovide_scale_factor = g:neovide_scale_factor * a:delta
-  endfunction
-
-  nnoremap <expr><C-=> ChangeScaleFactor(1.25)
-  nnoremap <expr><C--> ChangeScaleFactor(1/1.25)
-  let g:neovide_floating_blur_amount_x = 200.0
-  let g:neovide_floating_blur_amount_y = 200.0
-
-
-  "" let g:neovide_scroll_animation_length = 0.3
-  "" let g:neovide_hide_mouse_when_typing = v:true
-
-  "" let g:neovide_cursor_animation_length=0.05
-  "" let g:neovide_cursor_trail_size = 1.2
-  "" let g:neovide_cursor_antialiasing = v:true
-
-  let g:neovide_cursor_vfx_mode = "railgun"
-
-  "" let g:neovide_cursor_vfx_particle_density = 15
-  "" let g:neovide_cursor_vfx_particle_lifetime = 1.2
-  "" let g:neovide_cursor_vfx_particle_speed = 10.0
-  "" let g:neovide_transparency = 0.8
-  let g:neovide_confirm_quit = v:true
-  let g:neovide_fullscreen = v:false
-endif
 ]]
+
+if vim.g.neovide then
+  vim.o.guifont = "FiraCode Nerd Font:h14"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_floating_blur_amount_x = 200.0
+  vim.g.neovide_floating_blur_amount_y = 200.0
+  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_confirm_quit = false
+  vim.g.neovide_fullscreen = false
+  vim.cmd [[
+    function! ChangeScaleFactor(delta)
+        let g:neovide_scale_factor = g:neovide_scale_factor * a:delta
+    endfunction
+    nnoremap <expr><C-=> ChangeScaleFactor(1.25)
+    nnoremap <expr><C--> ChangeScaleFactor(1/1.25)
+  ]]
+end
 
 ------------------------------------------------------------
 -- Autosave ------------------------------------------------
 ------------------------------------------------------------
 
--- Enable Autosave using CursorHold
-vim.cmd [[
-    :set updatetime=300
-    :au CursorHold * :silent! wa
-]]
-
--- Write a function to toggle it
-local auto_save = true
-function ToggleAutoSave()
-  if auto_save then
-    vim.cmd [[ :au! ]]
-    auto_save = false
-    vim.cmd("echo 'Auto-save is OFF'")
-  else
-    vim.cmd [[ :au CursorHold * :silent! wa ]]
-    auto_save = true
-    vim.cmd("echo 'Auto-save is ON'")
-  end
-end
-map('n', '<C-s>', ':lua ToggleAutoSave()<CR>', { noremap = true, silent = true})
-
-
-vim.cmd [[
-set conceallevel=0
-]]
-
+-- TODO dead code
+-- -- Enable Autosave using CursorHold
+-- vim.cmd [[
+--     :set updatetime=300
+--     :au CursorHold * :silent! wa
+-- ]]
+--
+-- -- Write a function to toggle it
+-- local auto_save = true
+-- function ToggleAutoSave()
+--   if auto_save then
+--     vim.cmd [[ :au! ]]
+--     auto_save = false
+--     vim.cmd("echo 'Auto-save is OFF'")
+--   else
+--     vim.cmd [[ :au CursorHold * :silent! wa ]]
+--     auto_save = true
+--     vim.cmd("echo 'Auto-save is ON'")
+--   end
+-- end
+--
+--
+-- vim.cmd [[
+-- set conceallevel=0
+-- ]]
+--
 
 ------------------------------------------------------------
 -- File Extensions -----------------------------------------
 ------------------------------------------------------------
 vim.filetype.add({ extension = { txt = 'dokuwiki' } })
 vim.filetype.add({ extension = { elv = 'elvish' } })
-
