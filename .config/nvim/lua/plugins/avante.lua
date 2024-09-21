977f7625278d89355d5a2831c687ff9f48e8c343
ryan <exogenesis@protonmail.com>
2024-09-21 11:51:26 +1000

    nvim avante -- add multiple providers

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,35 +1,46 @@
+Parse_curl_args_func = function(opts, code_opts)
+  return {
+    url = opts.endpoint .. "/chat/completions",
+    headers = {
+      ["Accept"] = "application/json",
+      ["Content-Type"] = "application/json",
+    },
+    body = {
+      model = opts.model,
+      messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+      max_tokens = 2048,
+      stream = true,
+    },
+  }
+end
+
+Parse_response_data_func = function(data_stream, event_state, opts)
+  require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+end
+
+
+local create_ollama_config = function(host, model)
+  local url = string.format("http://%s:11434/v1", host)
+  return {
+    ["local"] = true,
+    endpoint = url,
+    model = model,
+    parse_curl_args = Parse_curl_args_func,
+    parse_response_data = Parse_response_data_func,
+  }
+end
+
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    provider = "ollama",
+    provider = "codestral",
     vendors = {
       ---@type AvanteProvider
-      ollama = {
-        ["local"] = true,
-        endpoint = "http://vale:11434/v1",
-        model = "codestral",
-        parse_curl_args = function(opts, code_opts)
-          return {
-            url = opts.endpoint .. "/chat/completions",
-            headers = {
-              ["Accept"] = "application/json",
-              ["Content-Type"] = "application/json",
-            },
-            body = {
-              model = opts.model,
-              messages = require("avante.providers").copilot.parse_message(code_opts),    -- you can make your own message, but this is very advanced
-              max_tokens = 2048,
-              stream = true,
-            },
-          }
-        end,
-        parse_response_data = function(data_stream, event_state, opts)
-          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-        end,
-      },
+      codestral = create_ollama_config("vale", "codestral"),
+      commandR = create_ollama_config("vale", "command-r"),
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
