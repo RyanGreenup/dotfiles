a11237f738a7b4ce2f30226b32b0f0baed7855ad
Ryan G <exogenesis@protonmail.com>
2025-09-27 13:31:25 +1000

    Add function to source env vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -177,6 +177,15 @@ function yy
 	rm -f -- "$tmp"
 end
 
+# https://gist.github.com/nikoheikkila/dd4357a178c8679411566ba2ca280fcc
+function envsource
+      for line in (cat .env | grep -v '^#')
+            set item (string split -m 1 '=' $line)
+            set -gx $item[1] $item[2]
+            echo "Exported key $item[1]"
+      end
+end
+
 # ..............................................................................
 # * Notetaking Stuff ...........................................................
 # ..............................................................................
