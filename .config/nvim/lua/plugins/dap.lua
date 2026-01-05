35cef357f2b94da23738bf9e5e094be3d511316c
Ryan G <exogenesis@protonmail.com>
2026-01-05 12:58:44 +1100

    comment out dap

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,15 +1,19 @@
-return {
-	"rcarriga/nvim-dap-ui",
-	dependencies = {
-		"mfussenegger/nvim-dap",
-		"nvim-neotest/nvim-nio",
-		"mfussenegger/nvim-dap-python",
-		{ "theHamsta/nvim-dap-virtual-text", opts = { enabled = true } },
-		"nvim-telescope/telescope-dap.nvim",
-	},
-	config = function()
-		require("dapui").setup()
-		require("dap-python").setup("~/.local/share/virtualenvs/debugpy/bin/python")
-		require("keymaps").dap_keymaps()
-	end,
-}
+-- return {
+-- 	"rcarriga/nvim-dap-ui",
+-- 	dependencies = {
+-- 		"mfussenegger/nvim-dap",
+-- 		"nvim-neotest/nvim-nio",
+-- 		"mfussenegger/nvim-dap-python",
+-- 		{ "theHamsta/nvim-dap-virtual-text", opts = { enabled = true } },
+-- 		"nvim-telescope/telescope-dap.nvim",
+-- 	},
+-- 	config = function()
+-- 		require("dapui").setup()
+-- 		require("dap-python").setup("~/.local/share/virtualenvs/debugpy/bin/python")
+-- 		require("keymaps").dap_keymaps()
+-- 	end,
+-- }
+
+-- TODO this needs to be fixed, low priority, VS Code and Zed debuggers are
+-- really good anyway
+return {}
