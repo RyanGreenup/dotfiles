511e308442b2d6b71ba0de5fbdd7b6a25d1efc18
Ryan Greenup <exogenesis@protonmail.com>
2026-03-28 15:26:57 +1100

    Merge settings

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -1,16 +1,44 @@
 --- Declarative user configuration for this Neovim setup.
 --- Centralises preferences that multiple modules may read.
+--- settings.lua applies vim options from here; plugin configs read their sections directly.
 return {
   ui = {
-    colorcolumn = '80',
+    number = true,
     relativenumber = false,
+    showmatch = true,
+    colorcolumn = '80',
     splitright = true,
     splitbelow = true,
+    linebreak = true,
+    termguicolors = true,
     yank_highlight_timeout = 700,
     gui_font = 'FiraCode Nerd Font:h14',
   },
+  editing = {
+    mouse = 'a',
+    clipboard = 'unnamedplus',
+    swapfile = false,
+    autoread = true,
+    ignorecase = true,
+    smartcase = true,
+    completeopt = 'menuone,noselect',
+  },
   indentation = {
     width = 4,
+    expandtab = true,
+    smartindent = true,
+  },
+  folding = {
+    method = 'expr',
+    expr = 'v:lua.vim.treesitter.foldexpr()',
+    level = 99,
+    levelstart = 99,
+  },
+  performance = {
+    hidden = true,
+    history = 100,
+    lazyredraw = false,
+    synmaxcol = 240,
   },
   behavior = {
     trim_whitespace = true,
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,101 +1,69 @@
 ------------------------------------------------------------
 -- Neovim Settings -----------------------------------------
 ------------------------------------------------------------
-
-------------------------------------------------------------
--- Aliases for Vim Commands---------------------------------
-------------------------------------------------------------
-
+-- Reads all tuneable values from lua/config.lua.
+-- This file only *applies* options and sets up autocommands/keymaps.
 
 local cfg = require('config')
-local map = vim.api.nvim_set_keymap -- set global keymap
-local cmd = vim.cmd                 -- execute Vim commands
-local exec = vim.api.nvim_exec      -- execute Vimscript
-local fn = vim.fn                   -- call Vim functions
-local g = vim.g                     -- global variables
-local opt = vim.opt                 -- global/buffer/windows-scoped options
+local cmd = vim.cmd
+local g = vim.g
+local opt = vim.opt
 
 -----------------------------------------------------------
--- General
+-- Leader
 -----------------------------------------------------------
-
-g.mapleader = ' '             -- change leader to a comma
+g.mapleader = ' '
 vim.api.nvim_set_keymap('i', '<M-Space>', '<Esc><leader>', { noremap = true })
-opt.mouse = 'a'               -- enable mouse support
-opt.clipboard = 'unnamedplus' -- copy/paste to system clipboard
-opt.swapfile = false          -- don't use swapfile
-cmd [[
-      imap jk <Esc>              " Get jk for Esc
-      ]] --
-
--- This is required for some FUSE and NFS stuff, see <https://github.com/RyanGreenup/joplin_sqlite_fuse/commit/6d03b93a26f645c76129b809161a34bddf2452b7>
-vim.cmd([[set backupcopy=yes]])
-
---------------------------------------------------------------------------------
--- Custom Vim Commands ---------------------------------------------------------
---------------------------------------------------------------------------------
--- Markdown Journal Insert Nav Table
-require('utils/insert_journal_table').setup()
 
 -----------------------------------------------------------
--- Neovim UI
+-- Editing
 -----------------------------------------------------------
-opt.number = true          -- show line number
-opt.showmatch = true       -- highlight matching parenthesis
-opt.foldlevelstart = 99    -- Open everything up at first
+opt.mouse = cfg.editing.mouse
+opt.clipboard = cfg.editing.clipboard
+opt.swapfile = cfg.editing.swapfile
+opt.autoread = cfg.editing.autoread
+opt.ignorecase = cfg.editing.ignorecase
+opt.smartcase = cfg.editing.smartcase
+opt.completeopt = cfg.editing.completeopt
+
+cmd [[imap jk <Esc>]]
+
+-- Required for FUSE/NFS, see https://github.com/RyanGreenup/joplin_sqlite_fuse/commit/6d03b93
+cmd [[set backupcopy=yes]]
+
+-----------------------------------------------------------
+-- UI
+-----------------------------------------------------------
+opt.number = cfg.ui.number
+opt.relativenumber = cfg.ui.relativenumber
+opt.showmatch = cfg.ui.showmatch
 opt.colorcolumn = cfg.ui.colorcolumn
 opt.splitright = cfg.ui.splitright
 opt.splitbelow = cfg.ui.splitbelow
-opt.ignorecase = true      -- ignore case letters when search
-opt.smartcase = true       -- ignore lowercase for the whole pattern
-opt.linebreak = true       -- wrap on word boundary
-opt.autoread = true        -- Automatically reload files
-opt.relativenumber = cfg.ui.relativenumber
+opt.linebreak = cfg.ui.linebreak
+opt.termguicolors = cfg.ui.termguicolors
+
 cmd [[ set modelineexpr ]] -- See vim modeline vulnerability 2019
 
+-----------------------------------------------------------
 -- Folding
-opt.foldmethod = "expr" -- fold based on Treesitter Expression
-opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- opt.foldtext = "v:lua.vim.treesitter.foldtext()" -- This is null if undefined, use autocommand if needed
-opt.foldlevel = 99                   -- Start with everything unfolded
-opt.fillchars:append({ fold = " " }) -- Don't show fold icons
-
--- remove whitespace on save
-if cfg.behavior.trim_whitespace then
-  cmd [[au BufWritePre * :%s/\s\+$//e]]
-end
-
-
--- highlight on yank
-exec([[
-    augroup YankHighlight
-      autocmd!
-      autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=]] .. cfg.ui.yank_highlight_timeout .. [[}
-    augroup end
-  ]], false)
-
+-----------------------------------------------------------
+opt.foldmethod = cfg.folding.method
+opt.foldexpr = cfg.folding.expr
+opt.foldlevel = cfg.folding.level
+opt.foldlevelstart = cfg.folding.levelstart
+opt.fillchars:append({ fold = " " })
 
 -----------------------------------------------------------
--- Memory, CPU
+-- Indentation
 -----------------------------------------------------------
-opt.hidden = true     -- enable background buffers
-opt.history = 100     -- remember n lines in history
-opt.lazyredraw = false -- faster scrolling
-opt.synmaxcol = 240   -- max column for syntax highlight
-
------------------------------------------------------------
--- Colorscheme
------------------------------------------------------------
-opt.termguicolors = true -- enable 24-bit RGB colors
-require('config.themes').setup()
-
------------------------------------------------------------
--- Tabs, indent
------------------------------------------------------------
-opt.expandtab = true   -- use spaces instead of tabs
+opt.expandtab = cfg.indentation.expandtab
 opt.shiftwidth = cfg.indentation.width
 opt.tabstop = cfg.indentation.width
-opt.smartindent = true -- autoindent new lines
+opt.smartindent = cfg.indentation.smartindent
+
+-- 2 spaces for selected filetypes
+cmd [[autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2]]
 
 -- don't auto commenting new lines
 cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
@@ -103,26 +71,38 @@ cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
 -- remove line length marker for selected filetypes
 cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]]
 
--- 2 spaces for selected filetypes
-cmd [[
-  autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2
-]]
+-----------------------------------------------------------
+-- Performance
+-----------------------------------------------------------
+opt.hidden = cfg.performance.hidden
+opt.history = cfg.performance.history
+opt.lazyredraw = cfg.performance.lazyredraw
+opt.synmaxcol = cfg.performance.synmaxcol
 
 -----------------------------------------------------------
--- Autocompletion
+-- Behavior — autocommands
 -----------------------------------------------------------
--- insert mode completion options
-opt.completeopt = 'menuone,noselect'
+if cfg.behavior.trim_whitespace then
+  cmd [[au BufWritePre * :%s/\s\+$//e]]
+end
+
+-- highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('YankHighlight', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = cfg.ui.yank_highlight_timeout })
+  end,
+})
+
+-----------------------------------------------------------
+-- Colorscheme
+-----------------------------------------------------------
+require('config.themes').setup()
 
 -----------------------------------------------------------
 -- Terminal
 -----------------------------------------------------------
--- open a terminal pane on the right using :Term
 cmd [[command Term :botright vsplit term://$SHELL]]
-
--- Terminal visual tweaks
---- enter insert mode when switching to terminal
---- close terminal buffer on process exit
 cmd [[
     autocmd TermOpen * setlocal listchars= nonumber norelativenumber nocursorline
     autocmd TermOpen * startinsert
@@ -132,33 +112,15 @@ cmd [[
 -----------------------------------------------------------
 -- Startup
 -----------------------------------------------------------
--- disable builtins plugins
 local disabled_built_ins = {
-  --  "netrw",
-  --  "netrwPlugin",
-  --  "netrwSettings",
-  --  "netrwFileHandlers",
-  "gzip",
-  "zip",
-  "zipPlugin",
-  "tar",
-  "tarPlugin",
-  "getscript",
-  "getscriptPlugin",
-  "vimball",
-  "vimballPlugin",
-  "2html_plugin",
-  "logipat",
-  "rrhelper",
-  "spellfile_plugin",
-  "matchit"
+  "gzip", "zip", "zipPlugin", "tar", "tarPlugin",
+  "getscript", "getscriptPlugin", "vimball", "vimballPlugin",
+  "2html_plugin", "logipat", "rrhelper", "spellfile_plugin", "matchit",
 }
-
 for _, plugin in pairs(disabled_built_ins) do
   g["loaded_" .. plugin] = 1
 end
 
--- disable nvim intro
 opt.shortmess:append "sI"
 
 -----------------------------------------------------------
@@ -167,37 +129,33 @@ opt.shortmess:append "sI"
 require('hlslens').setup()
 
 -----------------------------------------------------------
+-- Custom Commands
+-----------------------------------------------------------
+require('utils/insert_journal_table').setup()
+
+-----------------------------------------------------------
 -- Language Translation
 -----------------------------------------------------------
-
 function Line_to_esperanto()
-  -- Read the current line as a string
   local cl = vim.api.nvim_get_current_line()
-  -- Combine into a shell command
   local command = "argos-translate -f en -t eo '" .. cl .. "'"
-  -- Run the shell command and read the output as a string
   local out = io.popen(command):read()
-  -- Replace the line with the translation
   vim.api.nvim_set_current_line(out)
 end
 
-map('n', '<M-r>', ':lua Line_to_esperanto() <CR>', { noremap = true })
-
+vim.api.nvim_set_keymap('n', '<M-r>', ':lua Line_to_esperanto() <CR>', { noremap = true })
 
 -----------------------------------------------------------
--- Task Management Stuff
+-- Task Management
 -----------------------------------------------------------
 vim.cmd [[
 :command InsertTaskTags :r ! cd ~/Agenda/Agenda_Maybe && cat (fd -t f) | rg "^:([\w\s/]+):" -r '$1' -o | sort -u<CR>
 ]]
 
 -----------------------------------------------------------
--- Neoray GUI
+-- GUI — Neoray
 -----------------------------------------------------------
-
-
 vim.cmd [[
-
 if exists('g:neoray')
   NeoraySet CursorAnimTime 0.05
   NeoraySet Transparency 0.95
@@ -209,9 +167,11 @@ if exists('g:neoray')
   NeoraySet KeyZoomOut    <C-->
   set guifont=:12
 endif
-
 ]]
 
+-----------------------------------------------------------
+-- GUI — Neovide
+-----------------------------------------------------------
 if vim.g.neovide then
   vim.o.guifont = cfg.ui.gui_font
   vim.g.neovide_cursor_vfx_mode = "railgun"
